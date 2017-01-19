c1ec7c0f3328d47cd1ece26b02fc27070dedcad4
Brady <bradyehahn@icloud.com>
2017-01-19 14:11:56 -0600

    Updated README, Added an Example to src/test

---
+++ src/test/java/me/zero/example/ExampleClient.java
@@ -0,0 +1,14 @@
+package me.zero.example;
+
+import me.zero.api.client.Client;
+
+/**
+ * Created by Brady on 1/19/2017.
+ */
+public class ExampleClient extends Client {
+
+    @Override
+    public void init() {
+        // load
+    }
+}
