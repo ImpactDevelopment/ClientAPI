c1c6f41f6e4de9e21508ecbaa19c9286c310308d
Brady H <zeromemesdev@gmail.com>
2017-01-19 18:09:28 -0600

    Started Javadocs

--- src/test/java/me/zero/example/ExampleClient.java
+++ src/test/java/me/zero/example/ExampleClient.java
@@ -1,7 +1,6 @@
 package me.zero.example;
 
 import me.zero.client.api.Client;
-import me.zero.client.api.module.ModuleType;
 
 /**
  * Created by Brady on 1/19/2017.
@@ -10,16 +9,16 @@ public class ExampleClient extends Client {
 
     @Override
     public void preInit() {
-        ModuleType.create("Combat", "Render", "Movement", "Player", "World");
+        // Transformers and Module Types
     }
 
     @Override
     public void onInit() {
-
+        // Modules and UI
     }
 
     @Override
     public void postInit() {
-
+        // Nothing at the moment
     }
 }
