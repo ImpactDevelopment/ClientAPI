77384fc908ebd1010c3c97a5fe06fc1f1590268f
Brady <bradyehahn@icloud.com>
2017-01-22 11:05:48 -0600

    Module System Javadocs

--- src/main/java/me/zero/client/api/module/IModule.java
+++ src/main/java/me/zero/client/api/module/IModule.java
@@ -9,21 +9,63 @@ package me.zero.client.api.module;
  *
  * Created by Brady on 1/21/2017.
  */
-public interface IModule {
+interface IModule {
 
+    /**
+     * Called when the Module is enabled
+     *
+     * @since 1.0
+     */
     default void enable() {}
 
+    /**
+     * Called when the Module is disabled
+     *
+     * @since 1.0
+     */
     default void disable() {}
 
+    /**
+     * Used to toggle the state of the Module
+     *
+     * @since 1.0
+     */
     void toggle();
 
+    /**
+     * Directly sets the state of the Module
+     *
+     * @since 1.0
+     */
     void setState(boolean state);
 
+    /**
+     * Returns the current state
+     *
+     * @since 1.0
+     *
+     * @return The state of the Module
+     */
     boolean getState();
 
+    /**
+     * @since 1.0
+     *
+     * @return The name of the Module
+     */
     String getName();
 
+    /**
+     * @since 1.0
+     *
+     * @return The description of the Module
+     */
     String getDescription();
 
+    /**
+     * @since 1.0
+     *
+     * @return The type of the Module
+     */
     ModuleType getType();
 }
