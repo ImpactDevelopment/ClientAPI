c30c0309a24afca85a1db57727c91376f2320006
Brady <bradyehahn@icloud.com>
2017-01-21 21:23:23 -0600

    Bindable Javadocs

--- src/main/java/me/zero/client/api/util/IBindable.java
+++ src/main/java/me/zero/client/api/util/IBindable.java
@@ -6,18 +6,43 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * Marks a type as Bindable.
+ *
+ * All required methods are already
+ * inside of the interface.
+ *
+ * @since 1.0
+ *
  * Created by Brady on 1/21/2017.
  */
 public interface IBindable {
 
-    Map<IBindable, Integer> bindingMap = new HashMap<>();
+    /**
+     * Stores all of the binds for each instance of {@code IBindable}
+     */
+    Map<IBindable, Integer> BIND_MAP = new HashMap<>();
 
+    /**
+     * Sets the bind that corresponds with this
+     * implementation of {@code IBindable}
+     *
+     * @since 1.0
+     *
+     * @param bind The bind being set
+     */
     default void setBind(int bind) {
-        bindingMap.put(this, bind);
+        BIND_MAP.put(this, bind);
     }
 
+    /**
+     * Retrieves bind from the binding map
+     *
+     * @since 1.0
+     *
+     * @return The bind that belongs to this implementation
+     */
     default int getBind() {
-        bindingMap.putIfAbsent(this, Keyboard.KEY_NONE);
-        return bindingMap.get(this);
+        BIND_MAP.putIfAbsent(this, Keyboard.KEY_NONE);
+        return BIND_MAP.get(this);
     }
 }
