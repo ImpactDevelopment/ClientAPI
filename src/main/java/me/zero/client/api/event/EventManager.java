e4d275b3d24f9aef5b4f7b0299aecdfc83f59d70
Brady <bradyehahn@icloud.com>
2017-01-21 21:14:51 -0600

    Created Event API

--- src/main/java/me/zero/client/api/event/EventManager.java
+++ src/main/java/me/zero/client/api/event/EventManager.java
@@ -1,5 +1,8 @@
 package me.zero.client.api.event;
 
+import java.lang.reflect.Method;
+import java.util.*;
+
 /**
  * EventManager, Used to handle all Event Flow
  *
@@ -7,5 +10,67 @@ package me.zero.client.api.event;
  *
  * Created by Brady on 1/19/2017.
  */
-public class EventManager {
+public final class EventManager {
+
+    private static final Map<Class<?>, List<Listener>> SUBSCRIPTION_MAP = new HashMap<>();
+    private static final List<Listener> eventBuffer = new ArrayList<>();
+
+    public static void subscribe(Object object) {
+        for (Method method : object.getClass().getDeclaredMethods())
+            if (isValidMethod(method))
+                subscribe(object, method);
+    }
+
+    private static void subscribe(Object object, Method method) {
+        Class<?> eventClass = method.getParameterTypes()[0];
+        EventHandler eventHandler = method.getAnnotation(EventHandler.class);
+        Listener listener = new Listener(eventClass, object, method, eventHandler.value());
+
+        if (!method.isAccessible())
+            method.setAccessible(true);
+
+        subscribe(listener);
+    }
+
+    private static void subscribe(Listener listener){
+        eventBuffer.clear();
+
+        List<Listener> listeners = SUBSCRIPTION_MAP.get(listener.getType());
+        if (listeners != null && listeners.size() > 0)
+            eventBuffer.addAll(listeners);
+
+        int index = 0;
+        if (!eventBuffer.isEmpty()) {
+            for (; index < eventBuffer.size(); index++) {
+                if (listener.getPriority() < eventBuffer.get(index).getPriority()) {
+                    break;
+                }
+            }
+        }
+        eventBuffer.add(index, listener);
+        SUBSCRIPTION_MAP.put(listener.getType(), eventBuffer);
+    }
+
+    public static void unsubscribe(Object object) {
+        for (Class<?> eventClass : SUBSCRIPTION_MAP.keySet()) {
+            List<Listener> listeners = SUBSCRIPTION_MAP.get(eventClass);
+            eventBuffer.clear();
+            for (Listener listener : listeners) {
+                if (!listener.getParent().equals(object)) {
+                    eventBuffer.add(listener);
+                }
+            }
+            SUBSCRIPTION_MAP.put(eventClass, eventBuffer);
+        }
+    }
+
+    private static boolean isValidMethod(Method method) {
+        return method.isAnnotationPresent(EventHandler.class) && method.getParameterTypes().length == 1;
+    }
+
+    public static void post(Object event) {
+        List<Listener> listeners = SUBSCRIPTION_MAP.get(event.getClass());
+        if (listeners != null)
+            listeners.forEach(listener -> listener.invoke(event));
+    }
 }
