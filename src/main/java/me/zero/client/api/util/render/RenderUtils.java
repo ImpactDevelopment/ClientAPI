67632ca95bfc13d2933be3aa6a32785829431489
Brady <bradyehahn@icloud.com>
2017-02-06 17:52:11 -0600

    RenderUtils javadocs

--- src/main/java/me/zero/client/api/util/render/RenderUtils.java
+++ src/main/java/me/zero/client/api/util/render/RenderUtils.java
@@ -13,10 +13,27 @@ import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
  */
 public class RenderUtils {
 
+    /**
+     * Instance of the Tessellator
+     */
     private static Tessellator tessellator = Tessellator.getInstance();
+
+    /**
+     * Instance of the VertexBuffer
+     */
     private static VertexBuffer vertexBuffer = tessellator.getBuffer();
 
-    public static void drawFlippedTexturedModalRect(double x, double y, double x1, double y1) {
+    /**
+     * Draws a flipped textured rectangle
+     *
+     * @since 1.0
+     *
+     * @param x Top corner X of the rectangle
+     * @param y Top corner Y of the rectangle
+     * @param x1 Bottom corner X of the rectangle
+     * @param y1 Bottom corner Y of the rectangle
+     */
+    public static void drawFlippedTexturedRect(double x, double y, double x1, double y1) {
         vertexBuffer.begin(7, DefaultVertexFormats.POSITION_TEX);
         vertexBuffer.pos(x, y1, 0).tex(0, 0).endVertex();
         vertexBuffer.pos(x1, y1, 0).tex(1, 0).endVertex();
@@ -25,7 +42,16 @@ public class RenderUtils {
         tessellator.draw();
     }
 
-
+    /**
+     * Draws a reflected textured rectangle
+     *
+     * @since 1.0
+     *
+     * @param x Top corner X of the rectangle
+     * @param y Top corner Y of the rectangle
+     * @param x1 Bottom corner X of the rectangle
+     * @param y1 Bottom corner Y of the rectangle
+     */
     public static void drawReflectedTexturedRect(double x, double y, double x1, double y1) {
         vertexBuffer.begin(7, DefaultVertexFormats.POSITION_TEX);
         vertexBuffer.pos(x, y1, 0).tex(1, 0).endVertex();
