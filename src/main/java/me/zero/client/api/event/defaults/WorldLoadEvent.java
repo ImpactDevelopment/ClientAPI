0629193b3452cfe486e960ad1e113844721ded62
Brady <zeromemesdev@gmail.com>
2017-02-09 10:46:38 -0600

    Created a WorldLoadEvent

---
+++ src/main/java/me/zero/client/api/event/defaults/WorldLoadEvent.java
@@ -0,0 +1,23 @@
+package me.zero.client.api.event.defaults;
+
+import net.minecraft.client.multiplayer.WorldClient;
+
+/**
+ * Called when a world is loaded.
+ *
+ * @since 1.0
+ *
+ * Created by Brady on 2/9/2017.
+ */
+public class WorldLoadEvent {
+
+    private final WorldClient world;
+
+    public WorldLoadEvent(WorldClient world) {
+        this.world = world;
+    }
+
+    public WorldClient getWorld() {
+        return this.world;
+    }
+}
