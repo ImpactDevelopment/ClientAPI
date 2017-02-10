d1ccc959dcebf72fe095264783c2150539aa7811
Brady <bradyehahn@icloud.com>
2017-02-10 16:13:23 -0600

    Added the addCape and hasCape methods that were introduced in CapesAPI 2.1.0

--- src/main/java/me/zero/client/integration/defaults/CapesAPI.java
+++ src/main/java/me/zero/client/integration/defaults/CapesAPI.java
@@ -8,6 +8,9 @@ import net.minecraft.client.renderer.ThreadDownloadImageData;
 import net.minecraft.util.ResourceLocation;
 
 import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -22,11 +25,21 @@ import java.util.UUID;
  * @author Matthew Hatcher
  * @author Marco_MC
  * @author Brady
- * @version 2.0.0, Jan 2017
+ * @version 2.1.0, February 2017
  *
  * Created by Brady on 2/9/2017.
  */
 public class CapesAPI extends Integration implements Registry<UUID, ResourceLocation>, Helper {
+
+    /**
+     * The Current Version
+     */
+    public static final String VERSION = "2.1.0";
+
+    /**
+     * The CapesAPI Base URL
+     */
+    public static final String HOST = "http://capesapi.com/api/v1/";
 
     /**
      * Map containing all of the Capes
@@ -42,7 +55,7 @@ public class CapesAPI extends Integration implements Registry<UUID, ResourceLoca
      */
     @Override
     public void load(UUID obj) {
-        String url = "http://capesapi.com/api/v1/" + obj.toString() + "/getCape";
+        String url = HOST + obj.toString() + "/getCape";
         final ResourceLocation resourceLocation = new ResourceLocation("capeapi/capes/" + obj.toString() + ".png");
         IImageBuffer iImageBuffer = new IImageBuffer() {
 
@@ -93,5 +106,72 @@ public class CapesAPI extends Integration implements Registry<UUID, ResourceLoca
     @Override
     public boolean hasEntry(UUID obj) {
         return capes.containsKey(obj);
+    }
+
+    /**
+     * Returns whether or not the UUID has specified the capeId.
+     *
+     * @since 1.0
+     *
+     * @param uuid The UUID
+     * @param capeId The Cape ID
+     * @return Whether or not the UUID has the specified capeId
+     * @throws IOException thrown by URL Creation and various Streams
+     */
+    public boolean hasCape(UUID uuid, String capeId) throws IOException {
+        URL url = new URL(HOST + uuid.toString() + "/hasCape" + capeId);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+        String response = "";
+        String line;
+        while ((line = in.readLine()) != null)
+            response += line;
+
+        in.close();
+
+        return Integer.parseInt(response) == 1;
+    }
+
+    /**
+     * Grants the user associated with the UUID
+     * with the cape specified.
+     *
+     * @since 1.0
+     *
+     * @param uuid The UUID
+     * @param capeId The Cape ID
+     * @return Whether or not the cape adding was successful
+     * @throws IOException
+     */
+    public boolean addCape(UUID uuid, String capeId) throws IOException {
+        String data = "{\"capeId\": \"" + capeId + "\"}";
+        URL url = new URL(HOST + uuid.toString() + "/addCape");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.setDoOutput(true);
+        con.setDoInput(true);
+
+        DataOutputStream out = new DataOutputStream(con.getOutputStream());
+        out.write(data.getBytes());
+        out.flush();
+        out.close();
+
+        InputStream stream = con.getResponseCode() == 200 ? con.getInputStream() : con.getErrorStream();
+
+        if (stream == null)
+            throw new IOException();
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(stream));
+
+        String response = "";
+        String line;
+        while ((line = in.readLine()) != null)
+            response += line;
+
+        in.close();
+
+        return Integer.parseInt(response) == 1;
     }
 }
