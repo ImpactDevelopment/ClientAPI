e989a00140303c696e47e61988ddf28a5f11e054
Brady <bradyehahn@icloud.com>
2017-02-09 20:19:13 -0600

    Hooked KeyEvent

--- src/main/java/me/zero/client/api/event/handle/ClientHandler.java
+++ src/main/java/me/zero/client/api/event/handle/ClientHandler.java
@@ -2,13 +2,19 @@ package me.zero.client.api.event.handle;
 
 import me.zero.client.api.Client;
 import me.zero.client.api.event.EventHandler;
+import me.zero.client.api.event.EventManager;
 import me.zero.client.api.event.defaults.KeyEvent;
 import me.zero.client.api.event.defaults.Render2DEvent;
+import me.zero.client.api.event.defaults.TickEvent;
 import me.zero.client.api.exception.ActionNotValidException;
 import me.zero.client.api.module.Module;
 import me.zero.client.api.util.ClientUtils;
+import me.zero.client.api.util.interfaces.Helper;
 import me.zero.client.api.util.render.camera.CameraManager;
 import me.zero.client.load.ClientLoader;
+import org.lwjgl.input.Keyboard;
+
+import static org.lwjgl.input.Keyboard.KEYBOARD_SIZE;
 
 /**
  * Some basic events that the client uses
@@ -17,8 +23,9 @@ import me.zero.client.load.ClientLoader;
  *
  * Created by Brady on 2/9/2017.
  */
-public final class ClientHandler {
+public final class ClientHandler implements Helper {
 
+    private boolean[] keyMap = new boolean[KEYBOARD_SIZE];
     private Client client;
 
     public ClientHandler(Client client) {
@@ -36,5 +43,14 @@ public final class ClientHandler {
     @EventHandler
     private void onKey(KeyEvent event) {
         client.getModuleManager().getData().stream().filter(module -> module.getBind() == event.getKey()).forEach(Module::toggle);
+    }
+
+    @EventHandler
+    private void onTick(TickEvent event) {
+        if (mc.currentScreen != null) return;
+
+        for (int i = 1; i < KEYBOARD_SIZE; i++)
+            if (Keyboard.isKeyDown(i) != keyMap[i] && (keyMap[i] = !keyMap[i]))
+                EventManager.post(new KeyEvent(i));
     }
 }
