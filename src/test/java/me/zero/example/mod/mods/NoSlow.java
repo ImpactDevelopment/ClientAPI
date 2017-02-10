35fabb424da12d8974c68cda8b2f55b6e4362584
Brady <bradyehahn@icloud.com>
2017-02-10 09:49:34 -0600

    Created a NoSlow using the LivingUpdateEvent

---
+++ src/test/java/me/zero/example/mod/mods/NoSlow.java
@@ -0,0 +1,35 @@
+package me.zero.example.mod.mods;
+
+import me.zero.client.api.event.EventHandler;
+import me.zero.client.api.event.EventState;
+import me.zero.client.api.event.defaults.LivingUpdateEvent;
+import me.zero.client.api.module.Mod;
+import me.zero.client.api.module.Module;
+import net.minecraft.util.EnumHand;
+import org.lwjgl.input.Keyboard;
+
+/**
+ * Created by Brady on 2/10/2017.
+ */
+@Mod(name = "NoSlow", description = "While using items, You aren't slowed down", bind = Keyboard.KEY_N)
+public class NoSlow extends Module {
+
+    private EnumHand lastActive;
+
+    @EventHandler
+    private void onLivingUpdate(LivingUpdateEvent event) {
+        if (mc.player.isHandActive()) {
+            switch (event.getState()) {
+                case PRE: {
+                    lastActive = mc.player.getActiveHand();
+                    mc.player.resetActiveHand();
+                    break;
+                }
+                case POST: {
+                    mc.player.setActiveHand(lastActive);
+                    break;
+                }
+            }
+        }
+    }
+}
