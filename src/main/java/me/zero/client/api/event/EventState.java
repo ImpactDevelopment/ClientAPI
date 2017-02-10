fb6b00775c7562b193be1b916370ac14517b45ed
Brady <zeromemesdev@gmail.com>
2017-02-10 09:44:01 -0600

    Added an event for onLivingUpdate Event

---
+++ src/main/java/me/zero/client/api/event/EventState.java
@@ -0,0 +1,13 @@
+package me.zero.client.api.event;
+
+/**
+ * The state of an event
+ *
+ * @since 1.0
+ *
+ * Created by Brady on 2/10/2017.
+ */
+public enum EventState {
+
+    PRE, POST;
+}
---
+++ src/main/java/me/zero/client/api/event/defaults/LivingUpdateEvent.java
@@ -0,0 +1,23 @@
+package me.zero.client.api.event.defaults;
+
+import me.zero.client.api.event.EventState;
+
+/**
+ * Called before and after EntityPlayerSP#onLivingUpdate()
+ *
+ * @since 1.0
+ *
+ * Created by Brady on 2/10/2017.
+ */
+public final class LivingUpdateEvent {
+
+    private final EventState state;
+
+    public LivingUpdateEvent(EventState state) {
+        this.state = state;
+    }
+
+    public EventState getState() {
+        return this.state;
+    }
+}
