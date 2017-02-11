33d9af4ac1c7df53f403eb5cb7bc5cc4aa78ca33
Brady <zeromemesdev@gmail.com>
2017-02-10 21:12:56 -0600

    Added Nameable, and a better Keybind system, allowing virtually anything to have a keybind

--- src/main/java/me/zero/client/api/event/handle/ClientHandler.java
+++ src/main/java/me/zero/client/api/event/handle/ClientHandler.java
@@ -1,18 +1,17 @@
 package me.zero.client.api.event.handle;
 
-import me.zero.client.api.Client;
 import me.zero.client.api.event.EventHandler;
 import me.zero.client.api.event.EventManager;
 import me.zero.client.api.event.defaults.*;
 import me.zero.client.api.exception.ActionNotValidException;
-import me.zero.client.api.module.Module;
 import me.zero.client.api.util.ClientUtils;
 import me.zero.client.api.util.interfaces.Helper;
+import me.zero.client.api.util.keybind.Keybind;
 import me.zero.client.api.util.render.camera.CameraManager;
 import me.zero.client.load.ClientLoader;
-import net.minecraft.network.play.client.CPacketChatMessage;
-import net.minecraft.network.play.server.SPacketChat;
 import org.lwjgl.input.Keyboard;
+
+import java.util.stream.Stream;
 
 import static org.lwjgl.input.Keyboard.KEYBOARD_SIZE;
 
@@ -26,13 +25,10 @@ import static org.lwjgl.input.Keyboard.KEYBOARD_SIZE;
 public final class ClientHandler implements Helper {
 
     private boolean[] keyMap = new boolean[KEYBOARD_SIZE];
-    private Client client;
 
-    public ClientHandler(Client client) {
+    public ClientHandler() {
         if (ClientUtils.traceSource() != ClientLoader.class)
             throw new ActionNotValidException("The ClientHandler can only be initialized from the ClientLoader");
-
-        this.client = client;
     }
 
     @EventHandler
@@ -42,15 +38,25 @@ public final class ClientHandler implements Helper {
 
     @EventHandler
     private void onKey(KeyEvent event) {
-        client.getModuleManager().getData().stream().filter(module -> module.getBind() == event.getKey()).forEach(Module::toggle);
+        Keybind.getKeybinds().stream().filter(keybind -> keybind.getKey() == event.getKey()).forEach(Keybind::onClick);
     }
 
     @EventHandler
     private void onTick(TickEvent event) {
         if (mc.currentScreen != null) return;
 
-        for (int i = 1; i < KEYBOARD_SIZE; i++)
-            if (Keyboard.isKeyDown(i) != keyMap[i] && (keyMap[i] = !keyMap[i]))
-                EventManager.post(new KeyEvent(i));
+        for (int i = 1; i < KEYBOARD_SIZE; i++) {
+            final int key = i;
+            boolean currentState = Keyboard.isKeyDown(i);
+            if (currentState != keyMap[i]) {
+                if (keyMap[i] = !keyMap[i])
+                    EventManager.post(new KeyEvent(i));
+                Stream<Keybind> keybinds = Keybind.getKeybinds().stream().filter(keybind -> keybind.getKey() == key);
+                if (currentState)
+                    keybinds.forEach(Keybind::onPress);
+                else
+                    keybinds.forEach(Keybind::onRelease);
+            }
+        }
     }
 }
---
+++ src/main/java/me/zero/client/api/util/keybind/Keybind.java
@@ -0,0 +1,82 @@
+package me.zero.client.api.util.keybind;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A keybind that is used to create key hooks
+ *
+ * @since 1.0
+ *
+ * Created by Brady on 2/10/2017.
+ */
+public abstract class Keybind {
+
+    /**
+     * The List of all Keybind Objects
+     */
+    private static List<Keybind> keybinds = new ArrayList<>();
+
+    /**
+     * The KeyCode for this Keybind
+     */
+    private int key;
+
+    public Keybind(int key) {
+        this.key = key;
+        Keybind.keybinds.add(this);
+    }
+
+    /**
+     * @since 1.0
+     *
+     * @param key The key code being set
+     */
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    /**
+     * @since 1.0
+     *
+     * @return The key code of this bind
+     */
+    public int getKey() {
+        return this.key;
+    }
+
+    /**
+     * Called when a key's state has been
+     * switched from released to pressed
+     *
+     * @since 1.0
+     */
+    public abstract void onClick();
+
+    /**
+     * Called when a key is pressed
+     *
+     * @since 1.0
+     */
+    public abstract void onPress();
+
+    /**
+     * Claled when a key is released
+     *
+     * @since 1.0
+     */
+    public abstract void onRelease();
+
+    /**
+     * @since 1.0
+     *
+     * @return The list of all Keybind Objects
+     */
+    public static List<Keybind> getKeybinds() {
+        return Keybind.keybinds;
+    }
+
+    public enum Type {
+        TOGGLE, HOLD
+    }
+}
