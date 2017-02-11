0bd97d3d8183e5c6dfd76a1c8154293a812721bd
Brady <bradyehahn@icloud.com>
2017-02-11 12:20:42 -0600

    Added prediction method and updated EntityUtil javadocs

--- src/main/java/me/zero/client/api/util/EntityUtil.java
+++ src/main/java/me/zero/client/api/util/EntityUtil.java
@@ -5,7 +5,8 @@ import net.minecraft.util.math.Vec2f;
 import net.minecraft.util.math.Vec3d;
 
 /**
- * An
+ * A way to easily interact with entities without
+ * extra, unnecessary code.
  *
  * @since 1.0
  *
@@ -13,16 +14,40 @@ import net.minecraft.util.math.Vec3d;
  */
 public class EntityUtil {
 
+    /**
+     * Entity represented by this {@code EntityUtil}
+     */
     private Entity entity;
 
     public EntityUtil(Entity entity) {
         this.entity = entity;
     }
 
+    /**
+     * Sets all forms of position and rotation in
+     * this entity to the specified position and rotation
+     *
+     * @since 1.0
+     *
+     * @param pos The position
+     * @param angles The rotation
+     */
     public void setAll(Vec3d pos, Vec2f angles) {
         this.setAll(pos, pos, pos, angles, angles);
     }
 
+    /**
+     * Sets all forms of position of rotation to
+     * their specified vectors
+     *
+     * @since 1.0
+     *
+     * @param pos The position
+     * @param prev The previous position
+     * @param lastTick The last tick position
+     * @param angles The angles
+     * @param prevAngles The previous angles
+     */
     public void setAll(Vec3d pos, Vec3d prev, Vec3d lastTick, Vec2f angles, Vec2f prevAngles) {
         this.setPos(pos);
         this.setPrevPos(prev);
@@ -31,54 +56,144 @@ public class EntityUtil {
         this.setPrevRotations(prevAngles);
     }
 
+    /**
+     * Sets the entity position
+     *
+     * @since 1.0
+     *
+     * @param pos New position
+     */
     public void setPos(Vec3d pos) {
         entity.posX = pos.xCoord;
         entity.posY = pos.yCoord;
         entity.posZ = pos.zCoord;
     }
 
+    /**
+     * Sets the previous entity position
+     *
+     * @since 1.0
+     *
+     * @param pos New position
+     */
     public void setPrevPos(Vec3d pos) {
         entity.prevPosX = pos.xCoord;
         entity.prevPosY = pos.yCoord;
         entity.prevPosZ = pos.zCoord;
     }
 
+    /**
+     * Sets the last tick entity position
+     *
+     * @since 1.0
+     *
+     * @param pos New position
+     */
     public void setLastTickPos(Vec3d pos) {
         entity.lastTickPosX = pos.xCoord;
         entity.lastTickPosY = pos.yCoord;
         entity.lastTickPosZ = pos.zCoord;
     }
 
+    /**
+     * Sets the entity rotations
+     *
+     * @since 1.0
+     *
+     * @param rotations New position
+     */
     public void setRotations(Vec2f rotations) {
         entity.rotationYaw = rotations.x;
         entity.rotationPitch = rotations.y;
     }
 
+    /**
+     * Sets the previous entity rotations
+     *
+     * @since 1.0
+     *
+     * @param rotations New position
+     */
     public void setPrevRotations(Vec2f rotations) {
         entity.prevRotationYaw = rotations.x;
         entity.prevRotationPitch = rotations.y;
     }
 
+    /**
+     * Converts the entity position to a {@code Vec3d}
+     *
+     * @since 1.0
+     *
+     * @return Position as a {@code Vec3d}
+     */
     public Vec3d getPos() {
         return new Vec3d(entity.posX, entity.posY, entity.posZ);
     }
 
+    /**
+     * Converts the previous entity position to a {@code Vec3d}
+     *
+     * @since 1.0
+     *
+     * @return Previous position as a {@code Vec3d}
+     */
     public Vec3d getPrevPos() {
         return new Vec3d(entity.prevPosX, entity.prevPosY, entity.prevPosZ);
     }
 
+    /**
+     * Converts the last tick entity position to a {@code Vec3d}
+     *
+     * @since 1.0
+     *
+     * @return Last tick position as a {@code Vec3d}
+     */
     public Vec3d getLastTickPos() {
         return new Vec3d(entity.lastTickPosX, entity.lastTickPosY, entity.lastTickPosZ);
     }
 
+    /**
+     * Calculates the predicted position that an entity will
+     * be in the specified amount of ticks
+     *
+     * @since 1.0
+     *
+     * @param ticks Time in ticks predicted
+     * @return The predicted position
+     */
+    public Vec3d predictPos(int ticks) {
+        return getPos().add(getPos().subtract(getLastTickPos()).scale(ticks));
+    }
+
+    /**
+     * Converts the entity rotation angles to a {@code Vec2f}
+     *
+     * @since 1.0
+     *
+     * @return Rotations as a {@code Vec2f}
+     */
     public Vec2f getRotations() {
         return new Vec2f(entity.rotationYaw, entity.rotationPitch);
     }
 
+    /**
+     * Converts the previous entity rotation angles to a {@code Vec2f}
+     *
+     * @since 1.0
+     *
+     * @return Previous rotations as a {@code Vec2f}
+     */
     public Vec2f getPrevRotations() {
         return new Vec2f(entity.prevRotationYaw, entity.prevRotationPitch);
     }
 
+    /**
+     * Returns the {@code Entity} that this {@code EntityUtil} represents
+     *
+     * @since 1.0
+     *
+     * @return The Entity
+     */
     public Entity getEntity() {
         return this.entity;
     }
