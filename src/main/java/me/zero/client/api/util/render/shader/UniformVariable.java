c6f43ca665dca82dd142a4edf26fed222f69e79f
Brady <zeromemesdev@gmail.com>
2017-02-16 13:27:41 -0600

    Wrote a shader system, removed the CFont renderer

---
+++ src/main/java/me/zero/client/api/util/render/shader/UniformVariable.java
@@ -0,0 +1,50 @@
+package me.zero.client.api.util.render.shader;
+
+import me.zero.client.api.util.math.Vec2;
+import me.zero.client.api.util.math.Vec3;
+
+import static org.lwjgl.opengl.ARBShaderObjects.*;
+
+/**
+ * A representation of a GLSL Uniform Variable
+ *
+ * @since 1.0
+ *
+ * Created by Brady on 2/16/2017.
+ */
+public class UniformVariable {
+
+    private int location;
+
+    private UniformVariable(int location) {
+        this.location = location;
+    }
+
+    public void setInt(int value) {
+        glUniform1iARB(location, value);
+    }
+
+    public void setFloat(float value) {
+        glUniform1fARB(location, value);
+    }
+
+    public void setBoolean(boolean value) {
+        glUniform1fARB(location, value ? 1 : 0);
+    }
+
+    public void setVec(Vec2 value) {
+        glUniform2fARB(location, value.getX(), value.getY());
+    }
+
+    public void setVec(Vec3 value) {
+        glUniform3fARB(location, value.getX(), value.getY(), value.getZ());
+    }
+
+    public int getLocation() {
+        return this.location;
+    }
+
+    public static UniformVariable get(int shaderID, String uniformName) {
+        return new UniformVariable(glGetUniformLocationARB(shaderID, uniformName));
+    }
+}
--- src/test/java/me/zero/example/mod/mods/Hud.java
+++ src/test/java/me/zero/example/mod/mods/Hud.java
@@ -3,15 +3,13 @@ package me.zero.example.mod.mods;
 import me.zero.client.api.event.EventHandler;
 import me.zero.client.api.event.Listener;
 import me.zero.client.api.event.defaults.Render2DEvent;
-import me.zero.client.api.event.type.EventPriority;
-import me.zero.client.api.gui.font.CFontRenderer;
 import me.zero.client.api.module.Mod;
 import me.zero.client.api.module.Module;
 import me.zero.example.ExampleClient;
 import me.zero.example.mod.category.IRender;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.ScaledResolution;
 
-import java.awt.*;
 import java.util.Comparator;
 
 /**
@@ -19,8 +17,6 @@ import java.util.Comparator;
  */
 @Mod(name = "HUD", description = "Displays an In-Game HUD")
 public class Hud extends Module implements IRender {
-
-    private CFontRenderer font = new CFontRenderer(new Font("Caviar Dreams", Font.PLAIN, 24));
 
     private int y;
 
@@ -31,6 +27,7 @@ public class Hud extends Module implements IRender {
     @EventHandler
     private Listener<Render2DEvent> render2DListener = new Listener<>(event -> {
         ScaledResolution sr = new ScaledResolution(mc);
+        FontRenderer font = mc.fontRenderer;
 
         int color = 0xFF98ABB8;
 
@@ -41,7 +38,7 @@ public class Hud extends Module implements IRender {
         ExampleClient.getInstance().getModuleManager().getData().stream().filter(Module::getState)
                 .sorted(Comparator.comparingInt(m -> -font.getStringWidth(m.getName()))).forEach(module -> {
             font.drawStringWithShadow(module.getName(), sr.getScaledWidth() - 2 - font.getStringWidth(module.getName()), y, color);
-            y -= font.getHeight();
+            y -= font.FONT_HEIGHT;
         });
     });
 }
