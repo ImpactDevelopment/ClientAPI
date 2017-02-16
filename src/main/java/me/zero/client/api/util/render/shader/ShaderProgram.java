8952c33212439395a2de9c8ab37cf383557c0307
Brady <bradyehahn@icloud.com>
2017-02-16 13:27:41 -0600

    Wrote a shader system, removed the CFont renderer

---
+++ src/main/java/me/zero/client/api/util/render/shader/ShaderProgram.java
@@ -0,0 +1,79 @@
+package me.zero.client.api.util.render.shader;
+
+import me.zero.client.api.util.io.StreamReader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.lwjgl.opengl.ARBFragmentShader.GL_FRAGMENT_SHADER_ARB;
+import static org.lwjgl.opengl.ARBShaderObjects.*;
+import static org.lwjgl.opengl.ARBVertexShader.GL_VERTEX_SHADER_ARB;
+
+/**
+ * Used to create ARB Shader Programs with OpenGL
+ *
+ * @since 1.0
+ *
+ * Created by Brady on 2/16/2017.
+ */
+public abstract class ShaderProgram {
+
+    private List<UniformVariable> uniforms = new ArrayList<>();
+    private int programID, fragmentID, vertexID;
+
+    public ShaderProgram(String vertex, String fragment) {
+        programID = glCreateProgramObjectARB();
+        vertexID = loadShader(vertex, GL_VERTEX_SHADER_ARB);
+        fragmentID = loadShader(fragment, GL_FRAGMENT_SHADER_ARB);
+
+        attachShader(vertexID);
+        attachShader(fragmentID);
+
+        loadProgram();
+        loadUniforms();
+    }
+
+    public final void start() {
+        glUseProgramObjectARB(programID);
+    }
+
+    public final void stop() {
+        glUseProgramObjectARB(0);
+    }
+
+    public abstract void loadUniforms();
+
+    public final void loadUniform(String name) {
+        this.uniforms.add(UniformVariable.get(programID, name));
+    }
+
+    public final void cleanUp() {
+        stop();
+        glDetachObjectARB(programID, vertexID);
+        glDetachObjectARB(programID, fragmentID);
+        glDeleteObjectARB(vertexID);
+        glDeleteObjectARB(fragmentID);
+        glDeleteObjectARB(programID);
+    }
+
+    private int loadShader(String path, int type) {
+        String src = new StreamReader(ShaderProgram.class.getResourceAsStream(path)).read();
+        int shaderID = glCreateShaderObjectARB(type);
+        glShaderSourceARB(shaderID, src);
+        glCompileShaderARB(shaderID);
+        ShaderHelper.checkObjecti(shaderID, GL_OBJECT_COMPILE_STATUS_ARB);
+        return shaderID;
+    }
+
+    private void attachShader(int shaderID) {
+        glAttachObjectARB(programID, shaderID);
+    }
+
+    private void loadProgram() {
+        glLinkProgramARB(programID);
+        ShaderHelper.checkObjecti(programID, GL_OBJECT_LINK_STATUS_ARB);
+        glValidateProgramARB(programID);
+        ShaderHelper.checkObjecti(programID, GL_OBJECT_VALIDATE_STATUS_ARB);
+        stop();
+    }
+}
