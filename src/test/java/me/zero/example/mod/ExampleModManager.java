22b839f3a8e49d0f76880f08c9282b540c09c4b1
Brady <bradyehahn@icloud.com>
2017-02-17 20:29:20 -0600

    Removed NoSlow from Example Client ModuleManager and remove optional arguments from commands

--- src/test/java/me/zero/example/mod/ExampleModManager.java
+++ src/test/java/me/zero/example/mod/ExampleModManager.java
@@ -2,10 +2,12 @@ package me.zero.example.mod;
 
 import me.zero.client.api.manage.Manager;
 import me.zero.client.api.module.Module;
+import me.zero.client.api.util.Messages;
 import me.zero.client.api.util.logger.Level;
 import me.zero.client.api.util.logger.Logger;
 import me.zero.example.ExampleClient;
 import me.zero.example.mod.mods.*;
+import org.reflections.Reflections;
 
 /**
  * Created by Brady on 1/25/2017.
@@ -21,14 +23,13 @@ public class ExampleModManager extends Manager<Module> {
         Logger.instance.log(Level.INFO, "Loading Modules");
 
         // Load Modules
-        this.addData(
-                new Hud(),
-                new Fly(),
-                new NoSlow(),
-                new Speed(),
-                new Camera(),
-                new Aura()
-        );
+        new Reflections("me.zero.example.mod.mods").getSubTypesOf(Module.class).forEach(clazz -> {
+            try {
+                this.addData(clazz.newInstance());
+            } catch (InstantiationException | IllegalAccessException e) {
+                Logger.instance.logf(Level.SEVERE, Messages.MODULE_INSTANTIATION, clazz.getCanonicalName());
+            }
+        });
 
         // Loads Modules from the discovered Plugins by the Plugin loaders
         ExampleClient.getInstance().getPlugins().forEach(plugin -> this.addData(plugin.getModules()));
