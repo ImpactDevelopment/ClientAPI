269e192093dd4054ae0d52ef14c3c4c56fb6712a
Brady <zeromemesdev@gmail.com>
2017-02-23 14:39:40 -0600

    Created basic Wrapper system

--- src/main/java/me/zero/client/load/transformer/defaults/TRenderManager.java
+++ src/main/java/me/zero/client/load/transformer/defaults/TRenderManager.java
@@ -1,15 +1,10 @@
 package me.zero.client.load.transformer.defaults;
 
-import javassist.ClassPool;
-import javassist.CtClass;
-import javassist.CtNewMethod;
-import javassist.CtPrimitiveType;
-import me.zero.client.api.wrapper.IMinecraft;
-import me.zero.client.api.wrapper.IRenderManager;
 import me.zero.client.load.transformer.LoadTransformer;
 import me.zero.client.load.transformer.Transformer;
 import me.zero.client.load.transformer.hook.ClassHook;
 import me.zero.client.load.transformer.reference.ClassReference;
+import me.zero.client.load.transformer.wrapper.defaults.WRenderManager;
 
 import java.util.Collection;
 
@@ -27,12 +22,7 @@ public class TRenderManager extends Transformer {
 
     @Override
     public void loadHooks(Collection<ClassHook> hooks) {
-        hooks.add(ctClass -> {
-            ctClass.addInterface(ClassPool.getDefault().get(IRenderManager.class.getName()));
-            ctClass.addMethod(CtNewMethod.make(CtPrimitiveType.doubleType, "getRenderPosX", new CtClass[0], new CtClass[0], renderPosX.createReturn(), ctClass));
-            ctClass.addMethod(CtNewMethod.make(CtPrimitiveType.doubleType, "getRenderPosY", new CtClass[0], new CtClass[0], renderPosY.createReturn(), ctClass));
-            ctClass.addMethod(CtNewMethod.make(CtPrimitiveType.doubleType, "getRenderPosZ", new CtClass[0], new CtClass[0], renderPosZ.createReturn(), ctClass));
-        });
+        hooks.add(new WRenderManager().createClassHook());
     }
 
     @Override
