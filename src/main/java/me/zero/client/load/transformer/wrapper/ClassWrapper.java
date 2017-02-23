ec17554fe60e517fd1d8cb3340ac11269a73b9f5
Brady <zeromemesdev@gmail.com>
2017-02-23 17:47:54 -0600

    Updated Javadocs

--- src/main/java/me/zero/client/load/transformer/wrapper/ClassWrapper.java
+++ src/main/java/me/zero/client/load/transformer/wrapper/ClassWrapper.java
@@ -2,7 +2,7 @@ package me.zero.client.load.transformer.wrapper;
 
 import com.google.common.collect.Sets;
 import javassist.*;
-import me.zero.client.api.exception.UnexpectedOutcomeException;
+import me.zero.client.api.exception.ActionNotValidException;
 import me.zero.client.api.util.logger.Level;
 import me.zero.client.api.util.logger.Logger;
 import me.zero.client.load.transformer.hook.ClassHook;
@@ -23,34 +23,84 @@ import static me.zero.client.api.util.Messages.TRANSFORM_WRAPPER_COMPILE_METHOD;
  */
 public abstract class ClassWrapper {
 
+    /**
+     * Set of CtMethod representations of the Methods in the Wrapper Interface
+     */
     private Set<CtMethod> methods = Sets.newHashSet();
+
+    /**
+     * The target class being "wrapped"
+     */
     private CtClass target;
+
+    /**
+     * The Wrapper Interface
+     */
     private Class<?> wrapper;
 
     public ClassWrapper(ClassReference target, Class<?> wrapper) {
         this.wrapper = wrapper;
-        try {
-            this.target = target.getCtClass();
-        } catch (NotFoundException e) {
-            throw new UnexpectedOutcomeException("NotFoundException when getting CtClass from ClassReference!");
-        }
+        this.target = target.getCtClass();
         this.loadImplementations();
     }
 
+    /**
+     * Called to load all method implementations
+     *
+     * @since 1.0
+     */
     protected abstract void loadImplementations();
 
+    /**
+     * Implements a return method from a FieldReference
+     *
+     * @since 1.0
+     *
+     * @param methodName The name of the method
+     * @param returnType The return type of the method as a CtClass
+     */
     protected void implement(String methodName, CtClass returnType, FieldReference reference) {
         this.implement(methodName, returnType, reference.createReturn());
     }
 
+    /**
+     * Implements a method from its method data
+     *
+     * @since 1.0
+     *
+     * @param methodName The name of the method
+     * @param returnType The return type of the method as a CtClass
+     * @param src The source code of the method
+     */
     protected void implement(String methodName, CtClass returnType, String src) {
         this.implement(methodName, returnType, new CtClass[0], src);
     }
 
+    /**
+     * Implements a method from its method data
+     *
+     * @since 1.0
+     *
+     * @param methodName The name of the method
+     * @param returnType The return type of the method as a CtClass
+     * @param parameters An array of method parameters
+     * @param src The source code of the method
+     */
     protected void implement(String methodName, CtClass returnType, CtClass[] parameters, String src) {
         this.implement(methodName, returnType, parameters, new CtClass[0], src);
     }
 
+    /**
+     * Implements a method from its method data
+     *
+     * @since 1.0
+     *
+     * @param methodName The name of the method
+     * @param returnType The return type of the method as a CtClass
+     * @param parameters An array of method parameters
+     * @param exceptions An array of exceptions being thrown
+     * @param src The source code of the method
+     */
     protected void implement(String methodName, CtClass returnType, CtClass[] parameters, CtClass[] exceptions, String src) {
         try {
             this.implement(CtNewMethod.make(returnType, methodName, parameters, exceptions, src, target));
@@ -59,17 +109,39 @@ public abstract class ClassWrapper {
         }
     }
 
+    /**
+     * Implements a CtMethod
+     *
+     * @since 1.0
+     *
+     * @param method Method being implemented
+     */
     protected void implement(CtMethod method) {
         if (!methods.contains(method))
             methods.add(method);
     }
 
-    public boolean isImplComplete() {
+    /**
+     * @since 1.0
+     *
+     * @return Whether or not all methods have been implemented
+     */
+    private boolean isImplComplete() {
         // Basic check, change later
         return wrapper.getClass().getDeclaredMethods().length == methods.size();
     }
 
+    /**
+     * Creates a ClassHook from the Wrapper
+     *
+     * @since 1.0
+     *
+     * @return Created ClassHook
+     */
     public ClassHook createClassHook() {
+        if (isImplComplete())
+            throw new ActionNotValidException("ClassHook cannot be created if implementation isn't complete!");
+
         return ctClass -> {
             ctClass.addInterface(target);
             methods.forEach(method -> {
