6fef52f31e5086e66e9d970f16f94477c198086e
Brady <bradyehahn@icloud.com>
2017-02-23 19:19:58 -0600

    Created FileManager

---
+++ src/main/java/me/zero/client/api/util/io/FileManager.java
@@ -0,0 +1,115 @@
+package me.zero.client.api.util.io;
+
+import me.zero.client.api.util.logger.Level;
+import me.zero.client.api.util.logger.Logger;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Used for File I/O
+ *
+ * @since 1.0
+ *
+ * Created by Brady on 2/23/2017.
+ */
+public class FileManager {
+
+    /**
+     * Reads from a file and then returns a List
+     * containing all of the data from the file.
+     *
+     * @since 1.0
+     *
+     * @param file File being read from
+     * @return The list of data
+     */
+    public static List<String> read(String file) {
+        List<String> data = new ArrayList<>();
+        if (isAlreadyMade(file)) {
+            try {
+                FileReader fileReader = new FileReader(file);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    data.add(line);
+                }
+                bufferedReader.close();
+            } catch (IOException ex) {
+                Logger.instance.log(Level.WARNING, "Unable to load " + file);
+            }
+        } else {
+            createFile(file);
+        }
+        return data;
+    }
+
+    /**
+     * Writes data to a file, completely overriding the
+     * contents of the file.
+     *
+     * @since 1.0
+     *
+     * @param data Data being written
+     * @param file File being written to
+     */
+    public static void write(List<String> data, String file) {
+        if (!isAlreadyMade(file)) {
+            createFile(file);
+        }
+        try {
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+            for (String piece : data) {
+                bw.write(piece);
+                bw.newLine();
+            }
+            bw.close();
+        } catch (IOException e) {
+            Logger.instance.log(Level.WARNING, "Unable to write to " + file);
+        }
+    }
+
+    /**
+     * Creates a file from the specified file path
+     *
+     * @since 1.0
+     *
+     * @param file The file path
+     */
+    public static void createFile(String file) {
+        File theFile = new File(file);
+        Path path = Paths.get(file);
+        Path parent = Paths.get(theFile.getParent());
+        if (!Files.exists(parent)) {
+            try {
+                Files.createDirectory(parent);
+            } catch (IOException e) {
+                Logger.instance.log(Level.WARNING, "Unable to create " + theFile.getParent());
+            }
+        }
+        try {
+            Files.createFile(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Logger.instance.log(Level.WARNING, "Unable to create " + file);
+        }
+    }
+
+    /**
+     * Checks if the specified file path belongs
+     * to a file.
+     *
+     * @since 1.0
+     *
+     * @param file The file path
+     * @return Whether or not the file exists
+     */
+    private static boolean isAlreadyMade(String file) {
+        return Files.exists(Paths.get(file));
+    }
+}
