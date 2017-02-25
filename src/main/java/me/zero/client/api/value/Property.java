830f8f9c4b9b337005f784ce17501160ee31b60b
Brady <zeromemesdev@gmail.com>
2017-02-25 11:41:33 -0600

    Created Module modes and properties

---
+++ src/main/java/me/zero/client/api/value/Property.java
@@ -0,0 +1,64 @@
+package me.zero.client.api.value;
+
+/**
+ * Properties are similar to values but are not limited to Fields.
+ * Used in Nodes to handle unique properties of the Node.
+ *
+ * @since 1.0
+ *
+ * Created by Brady on 2/25/2017.
+ */
+public class Property {
+
+    /**
+     * The label of this property
+     */
+    private String label;
+
+    /**
+     * The value of this property
+     */
+    private Object value;
+
+    public Property(String label) {
+        this.label = label;
+    }
+
+    /**
+     * @since 1.0
+     *
+     * @return The label of this property
+     */
+    public String getLabel() {
+        return this.label;
+    }
+
+    /**
+     * @since 1.0
+     *
+     * @return The value of this property
+     */
+    public Object getValue() {
+        return this.value;
+    }
+
+    /**
+     * @since 1.0
+     *
+     * @return The value of this property as a string
+     */
+    public String getString() {
+        return value.toString();
+    }
+
+    /**
+     * Sets the value of this property to the specified object
+     *
+     * @since 1.0
+     *
+     * @param value New value
+     */
+    public void setValue(Object value) {
+        this.value = value;
+    }
+}
