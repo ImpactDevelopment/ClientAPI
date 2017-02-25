9199e0250ce00303f85116376d55212ee625b75d
Brady <zeromemesdev@gmail.com>
2017-02-24 20:39:13 -0600

    Added Timer util

---
+++ src/main/java/me/zero/client/api/util/Timer.java
@@ -0,0 +1,48 @@
+package me.zero.client.api.util;
+
+import me.zero.client.api.value.type.NumberType;
+
+/**
+ * Basic timer to keep track of tasks
+ *
+ * @since 1.0
+ *
+ * Created by Brady on 2/24/2017.
+ */
+public class Timer {
+
+    private long prevMS;
+
+    public final void reset() {
+        prevMS = System.currentTimeMillis();
+    }
+
+    public final long getDiff() {
+        return getTime() - prevMS;
+    }
+
+    public final boolean delay(long milliseconds) {
+        return getTime() >= prevMS + milliseconds;
+    }
+
+    public final boolean delay(NumberType milliseconds) {
+        return delay(milliseconds.getValue().intValue());
+    }
+
+    public final boolean delay(float milliSec) {
+        return (float) (getTime() - this.prevMS) >= milliSec;
+    }
+
+    public final boolean speed(float speed) {
+        speed = Math.max(0, speed);
+        return getTime() >= prevMS + (long) (1000 / speed);
+    }
+
+    public final boolean speed(NumberType speed) {
+        return this.speed(speed.getValue().floatValue());
+    }
+
+    private long getTime() {
+        return System.currentTimeMillis();
+    }
+}
