41f3f78d06a99de45674fc2e1594cdac4d2af7fd
Brady <bradyehahn@icloud.com>
2017-02-25 12:13:51 -0600

    Added round and random method to math utils

--- src/main/java/me/zero/client/api/util/MathUtils.java
+++ src/main/java/me/zero/client/api/util/MathUtils.java
@@ -1,5 +1,9 @@
 package me.zero.client.api.util;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Random;
+
 /**
  * Utils used for mathematical operations.
  *
@@ -28,5 +32,41 @@ public class MathUtils {
         if (value.doubleValue() > maximum.doubleValue()) value = maximum;
         if (value.doubleValue() < minimum.doubleValue()) value = minimum;
         return value;
+    }
+
+    /**
+     * Rounds a number to a specific place
+     *
+     * @since 1.0
+     *
+     * @param value Value being rounded
+     * @param places Places being rounded
+     * @return Rounded value
+     */
+    public static double roundToPlace(double value, int places) {
+        if (places < 0)
+            throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
+    /**
+     * Gets a random int between a min value and max value
+     *
+     * @since 1.0
+     *
+     * @param min The minimum value
+     * @param max The maximum value
+     * @return The random vakue
+     */
+    public static int randInt(int min, int max) {
+        if (min > max) {
+            int temp = max;
+            max = min;
+            min = temp;
+        }
+        return new Random().nextInt(max - min + 1) + min;
     }
 }
