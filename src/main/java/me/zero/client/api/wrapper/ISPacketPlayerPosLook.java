1ec01b82ac7f330e5ae763cdeee12aebde8d615c
Brady <bradyehahn@icloud.com>
2017-02-28 18:45:20 -0600

    Minor Fix

--- src/main/java/me/zero/client/api/wrapper/ISPacketPlayerPosLook.java
+++ src/main/java/me/zero/client/api/wrapper/ISPacketPlayerPosLook.java
@@ -7,4 +7,50 @@ package me.zero.client.api.wrapper;
  *
  * Created by Brady on 2/28/2017.
  */
-public interface ISPacketPlayerPosLook extends ICPacketPlayer {}
+public interface ISPacketPlayerPosLook {
+
+    /**
+     * Sets the packet X coordinate
+     *
+     * @since 1.0
+     *
+     * @param x X coordinate
+     */
+    void setX(double x);
+
+    /**
+     * Sets the packet Y coordinate
+     *
+     * @since 1.0
+     *
+     * @param y Y coordinate
+     */
+    void setY(double y);
+
+    /**
+     * Sets the packet Z coordinate
+     *
+     * @since 1.0
+     *
+     * @param z Z coordinate
+     */
+    void setZ(double z);
+
+    /**
+     * Sets the packet yaw rotation
+     *
+     * @since 1.0
+     *
+     * @param yaw Yaw rotation
+     */
+    void setYaw(float yaw);
+
+    /**
+     * Sets the packet pitch rotation
+     *
+     * @since 1.0
+     *
+     * @param pitch Pitch rotation
+     */
+    void setPitch(float pitch);
+}
