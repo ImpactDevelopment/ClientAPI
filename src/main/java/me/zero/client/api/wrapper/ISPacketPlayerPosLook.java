aa60afe917cd4de089e3c2d207f240fc7dc3dc6f
Brady <bradyehahn@icloud.com>
2017-02-28 18:43:27 -0600

    Created SPacketPlayerPosLook Wrapper

---
+++ src/main/java/me/zero/client/api/wrapper/ISPacketPlayerPosLook.java
@@ -0,0 +1,10 @@
+package me.zero.client.api.wrapper;
+
+/**
+ * Used to set the Server Player Packet fields
+ *
+ * @since 1.0
+ *
+ * Created by Brady on 2/28/2017.
+ */
+public interface ISPacketPlayerPosLook extends ICPacketPlayer {}
