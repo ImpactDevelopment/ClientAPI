8d7358de8733c11ab75707484278051edd0d3abd
Brady <zeromemesdev@gmail.com>
2017-03-01 12:06:10 -0600

    Added partialTicks to the Render 3D Event

--- src/main/java/me/zero/client/api/event/defaults/Render3DEvent.java
+++ src/main/java/me/zero/client/api/event/defaults/Render3DEvent.java
@@ -1,5 +1,8 @@
 package me.zero.client.api.event.defaults;
 
+import me.zero.client.api.util.interfaces.Helper;
+import me.zero.client.api.wrapper.IMinecraft;
+
 /**
  * Called at the end of EntityRenderer#renderWorldPass(int, float, long)
  *
@@ -7,4 +10,15 @@ package me.zero.client.api.event.defaults;
  *
  * Created by Brady on 2/9/2017.
  */
-public final class Render3DEvent {}
+public final class Render3DEvent implements Helper {
+
+    private final float partialTicks;
+
+    private Render3DEvent() {
+        this.partialTicks = ((IMinecraft) mc).getTimer().renderPartialTicks;
+    }
+
+    public float getPartialTicks() {
+        return this.partialTicks;
+    }
+}
