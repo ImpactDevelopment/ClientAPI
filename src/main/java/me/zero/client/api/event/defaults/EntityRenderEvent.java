109e5bc354ac526b97a9e068512c9d752c4dd00c
Brady <bradyehahn@icloud.com>
2017-03-02 16:57:06 -0600

    Fixed Render3DEvent, added EntityRenderEvent

---
+++ src/main/java/me/zero/client/api/event/defaults/EntityRenderEvent.java
@@ -0,0 +1,64 @@
+package me.zero.client.api.event.defaults;
+
+import me.zero.client.api.event.type.EventState;
+import net.minecraft.client.renderer.entity.RenderLivingBase;
+import net.minecraft.entity.Entity;
+
+/**
+ * Called before and after an entity is rendered
+ *
+ * @since 1.0
+ *
+ * Created by Brady on 3/2/2017.
+ */
+public class EntityRenderEvent {
+
+    /**
+     * The state of this Event
+     */
+    private EventState state;
+
+    /**
+     * The renderer used
+     */
+    private RenderLivingBase<?> renderer;
+
+    /**
+     * The entity being rendered
+     */
+    private Entity entity;
+
+    public EntityRenderEvent(EventState state, RenderLivingBase<?> renderer, Entity entity) {
+        this.state = state;
+        this.renderer = renderer;
+        this.entity = entity;
+    }
+
+    /**
+     * @since 1.0
+     *
+     * @return The event state
+     */
+    public EventState getState() {
+        return this.state;
+    }
+
+
+    /**
+     * @since 1.0
+     *
+     * @return The renderer used
+     */
+    public RenderLivingBase<?> getRenderer() {
+        return this.renderer;
+    }
+
+    /**
+     * @since 1.0
+     *
+     * @return The entity being rendered
+     */
+    public Entity getEntity() {
+        return this.entity;
+    }
+}
