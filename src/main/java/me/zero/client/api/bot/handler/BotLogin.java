795d92341ea5d7e951f8d6786491c40c03a1571b
Brady <zeromemesdev@gmail.com>
2017-03-07 14:13:07 -0600

    More bot stuff

--- src/main/java/me/zero/client/api/bot/handler/BotLogin.java
+++ src/main/java/me/zero/client/api/bot/handler/BotLogin.java
@@ -5,13 +5,8 @@ import com.mojang.authlib.exceptions.AuthenticationUnavailableException;
 import com.mojang.authlib.exceptions.InvalidCredentialsException;
 import com.mojang.authlib.minecraft.MinecraftSessionService;
 import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
-import edu.umd.cs.findbugs.annotations.ReturnValuesAreNonnullByDefault;
-import io.netty.util.concurrent.Future;
-import io.netty.util.concurrent.GenericFutureListener;
-import mcp.MethodsReturnNonnullByDefault;
 import me.zero.client.api.bot.Bot;
 import me.zero.client.api.util.interfaces.Helper;
-import net.minecraft.client.network.NetHandlerLoginClient;
 import net.minecraft.network.EnumConnectionState;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.login.INetHandlerLoginClient;
@@ -40,9 +35,19 @@ import java.util.UUID;
  */
 public class BotLogin implements INetHandlerLoginClient, Helper {
 
+    /**
+     * Bot Minecraft Session Service, initialized in the static constructor
+     */
     private static final MinecraftSessionService sessionService;
 
+    /**
+     * NetworkManager used by the Bot
+     */
     private final NetworkManager networkManager;
+
+    /**
+     * Bot that represents this Play Network Handler
+     */
     private final Bot bot;
 
     static {
@@ -64,20 +69,25 @@ public class BotLogin implements INetHandlerLoginClient, Helper {
 
         String encryptedid = new BigInteger(CryptManager.getServerIdHash(serverid, publickey, secretkey)).toString(16);
 
+        boolean success = false;
+
         try {
             sessionService.joinServer(bot.getSession().getProfile(), bot.getSession().getToken(), encryptedid);
+            success = true;
         } catch (AuthenticationUnavailableException e) {
             this.networkManager.closeChannel(new TextComponentTranslation("disconnect.loginFailedInfo", new TextComponentTranslation("disconnect.loginFailedInfo.serversUnavailable")));
-            return;
         } catch (InvalidCredentialsException e) {
             this.networkManager.closeChannel(new TextComponentTranslation("disconnect.loginFailedInfo", new TextComponentTranslation("disconnect.loginFailedInfo.invalidSession")));
-            return;
         } catch (AuthenticationException e) {
             this.networkManager.closeChannel(new TextComponentTranslation("disconnect.loginFailedInfo", e.getMessage()));
-            return;
         }
 
-        this.networkManager.sendPacket(new CPacketEncryptionResponse(secretkey, publickey, packetIn.getVerifyToken()), p_operationComplete_1_ -> BotLogin.this.networkManager.enableEncryption(secretkey));
+        if (!success)
+            return;
+
+        // noinspection unchecked
+        this.networkManager.sendPacket(new CPacketEncryptionResponse(secretkey, publickey, packetIn.getVerifyToken()),
+                future -> BotLogin.this.networkManager.enableEncryption(secretkey));
     }
 
     @Override
--- src/main/java/me/zero/client/api/bot/handler/BotPlay.java
+++ src/main/java/me/zero/client/api/bot/handler/BotPlay.java
@@ -1,6 +1,7 @@
 package me.zero.client.api.bot.handler;
 
 import me.zero.client.api.bot.Bot;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.network.play.INetHandlerPlayClient;
 import net.minecraft.network.play.server.*;
 import net.minecraft.util.text.ITextComponent;
@@ -16,6 +17,9 @@ import javax.annotation.ParametersAreNonnullByDefault;
  */
 public class BotPlay implements INetHandlerPlayClient {
 
+    /**
+     * Bot that represents this Play Network Handler
+     */
     private final Bot bot;
 
     public BotPlay(Bot bot) {
@@ -24,445 +28,303 @@ public class BotPlay implements INetHandlerPlayClient {
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleSpawnObject(SPacketSpawnObject packetIn) {
-
-    }
+    public void handleSpawnObject(SPacketSpawnObject packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleSpawnExperienceOrb(SPacketSpawnExperienceOrb packetIn) {
-
-    }
+    public void handleSpawnExperienceOrb(SPacketSpawnExperienceOrb packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleSpawnGlobalEntity(SPacketSpawnGlobalEntity packetIn) {
-
-    }
+    public void handleSpawnGlobalEntity(SPacketSpawnGlobalEntity packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleSpawnMob(SPacketSpawnMob packetIn) {
-
-    }
+    public void handleSpawnMob(SPacketSpawnMob packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleScoreboardObjective(SPacketScoreboardObjective packetIn) {
-
-    }
+    public void handleScoreboardObjective(SPacketScoreboardObjective packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleSpawnPainting(SPacketSpawnPainting packetIn) {
-
-    }
+    public void handleSpawnPainting(SPacketSpawnPainting packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleSpawnPlayer(SPacketSpawnPlayer packetIn) {
-
-    }
+    public void handleSpawnPlayer(SPacketSpawnPlayer packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleAnimation(SPacketAnimation packetIn) {
-
-    }
+    public void handleAnimation(SPacketAnimation packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleStatistics(SPacketStatistics packetIn) {
-
-    }
+    public void handleStatistics(SPacketStatistics packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleBlockBreakAnim(SPacketBlockBreakAnim packetIn) {
-
-    }
+    public void handleBlockBreakAnim(SPacketBlockBreakAnim packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleSignEditorOpen(SPacketSignEditorOpen packetIn) {
-
-    }
+    public void handleSignEditorOpen(SPacketSignEditorOpen packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleUpdateTileEntity(SPacketUpdateTileEntity packetIn) {
-
-    }
+    public void handleUpdateTileEntity(SPacketUpdateTileEntity packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleBlockAction(SPacketBlockAction packetIn) {
-
-    }
+    public void handleBlockAction(SPacketBlockAction packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleBlockChange(SPacketBlockChange packetIn) {
-
-    }
+    public void handleBlockChange(SPacketBlockChange packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleChat(SPacketChat packetIn) {
-
-    }
+    public void handleChat(SPacketChat packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleTabComplete(SPacketTabComplete packetIn) {
-
-    }
+    public void handleTabComplete(SPacketTabComplete packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleMultiBlockChange(SPacketMultiBlockChange packetIn) {
-
-    }
+    public void handleMultiBlockChange(SPacketMultiBlockChange packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleMaps(SPacketMaps packetIn) {
-
-    }
+    public void handleMaps(SPacketMaps packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleConfirmTransaction(SPacketConfirmTransaction packetIn) {
-
-    }
+    public void handleConfirmTransaction(SPacketConfirmTransaction packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleCloseWindow(SPacketCloseWindow packetIn) {
-
-    }
+    public void handleCloseWindow(SPacketCloseWindow packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleWindowItems(SPacketWindowItems packetIn) {
-
-    }
+    public void handleWindowItems(SPacketWindowItems packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleOpenWindow(SPacketOpenWindow packetIn) {
-
-    }
+    public void handleOpenWindow(SPacketOpenWindow packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleWindowProperty(SPacketWindowProperty packetIn) {
-
-    }
+    public void handleWindowProperty(SPacketWindowProperty packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleSetSlot(SPacketSetSlot packetIn) {
-
-    }
+    public void handleSetSlot(SPacketSetSlot packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleCustomPayload(SPacketCustomPayload packetIn) {
-
-    }
+    public void handleCustomPayload(SPacketCustomPayload packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleDisconnect(SPacketDisconnect packetIn) {
-
-    }
+    public void handleDisconnect(SPacketDisconnect packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleUseBed(SPacketUseBed packetIn) {
-
-    }
+    public void handleUseBed(SPacketUseBed packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleEntityStatus(SPacketEntityStatus packetIn) {
-
-    }
+    public void handleEntityStatus(SPacketEntityStatus packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleEntityAttach(SPacketEntityAttach packetIn) {
-
-    }
+    public void handleEntityAttach(SPacketEntityAttach packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleSetPassengers(SPacketSetPassengers packetIn) {
-
-    }
+    public void handleSetPassengers(SPacketSetPassengers packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleExplosion(SPacketExplosion packetIn) {
-
-    }
+    public void handleExplosion(SPacketExplosion packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleChangeGameState(SPacketChangeGameState packetIn) {
-
-    }
+    public void handleChangeGameState(SPacketChangeGameState packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleKeepAlive(SPacketKeepAlive packetIn) {
-
-    }
+    public void handleKeepAlive(SPacketKeepAlive packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleChunkData(SPacketChunkData packetIn) {
-
-    }
+    public void handleChunkData(SPacketChunkData packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void processChunkUnload(SPacketUnloadChunk packetIn) {
-
-    }
+    public void processChunkUnload(SPacketUnloadChunk packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleEffect(SPacketEffect packetIn) {
-
-    }
+    public void handleEffect(SPacketEffect packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
     public void handleJoinGame(SPacketJoinGame packetIn) {
-
+        // Create new Player
+        EntityPlayerSP player = bot.getPlayer();
+        player.dimension = packetIn.getDimension();
+        player.setEntityId(packetIn.getPlayerId());
+        player.setReducedDebug(packetIn.isReducedDebugInfo());
     }
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleEntityMovement(SPacketEntity packetIn) {
-
-    }
+    public void handleEntityMovement(SPacketEntity packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handlePlayerPosLook(SPacketPlayerPosLook packetIn) {
-
-    }
+    public void handlePlayerPosLook(SPacketPlayerPosLook packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleParticles(SPacketParticles packetIn) {
-
-    }
+    public void handleParticles(SPacketParticles packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handlePlayerAbilities(SPacketPlayerAbilities packetIn) {
-
-    }
+    public void handlePlayerAbilities(SPacketPlayerAbilities packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handlePlayerListItem(SPacketPlayerListItem packetIn) {
-
-    }
+    public void handlePlayerListItem(SPacketPlayerListItem packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleDestroyEntities(SPacketDestroyEntities packetIn) {
-
-    }
+    public void handleDestroyEntities(SPacketDestroyEntities packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleRemoveEntityEffect(SPacketRemoveEntityEffect packetIn) {
-
-    }
+    public void handleRemoveEntityEffect(SPacketRemoveEntityEffect packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleRespawn(SPacketRespawn packetIn) {
-
-    }
+    public void handleRespawn(SPacketRespawn packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleEntityHeadLook(SPacketEntityHeadLook packetIn) {
-
-    }
+    public void handleEntityHeadLook(SPacketEntityHeadLook packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleHeldItemChange(SPacketHeldItemChange packetIn) {
-
-    }
+    public void handleHeldItemChange(SPacketHeldItemChange packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleDisplayObjective(SPacketDisplayObjective packetIn) {
-
-    }
+    public void handleDisplayObjective(SPacketDisplayObjective packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleEntityMetadata(SPacketEntityMetadata packetIn) {
-
-    }
+    public void handleEntityMetadata(SPacketEntityMetadata packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleEntityVelocity(SPacketEntityVelocity packetIn) {
-
-    }
+    public void handleEntityVelocity(SPacketEntityVelocity packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleEntityEquipment(SPacketEntityEquipment packetIn) {
-
-    }
+    public void handleEntityEquipment(SPacketEntityEquipment packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleSetExperience(SPacketSetExperience packetIn) {
-
-    }
+    public void handleSetExperience(SPacketSetExperience packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleUpdateHealth(SPacketUpdateHealth packetIn) {
-
-    }
+    public void handleUpdateHealth(SPacketUpdateHealth packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleTeams(SPacketTeams packetIn) {
-
-    }
+    public void handleTeams(SPacketTeams packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleUpdateScore(SPacketUpdateScore packetIn) {
-
-    }
+    public void handleUpdateScore(SPacketUpdateScore packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleSpawnPosition(SPacketSpawnPosition packetIn) {
-
-    }
+    public void handleSpawnPosition(SPacketSpawnPosition packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleTimeUpdate(SPacketTimeUpdate packetIn) {
-
-    }
+    public void handleTimeUpdate(SPacketTimeUpdate packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleSoundEffect(SPacketSoundEffect packetIn) {
-
-    }
+    public void handleSoundEffect(SPacketSoundEffect packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleCustomSound(SPacketCustomSound packetIn) {
-
-    }
+    public void handleCustomSound(SPacketCustomSound packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleCollectItem(SPacketCollectItem packetIn) {
-
-    }
+    public void handleCollectItem(SPacketCollectItem packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleEntityTeleport(SPacketEntityTeleport packetIn) {
-
-    }
+    public void handleEntityTeleport(SPacketEntityTeleport packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleEntityProperties(SPacketEntityProperties packetIn) {
-
-    }
+    public void handleEntityProperties(SPacketEntityProperties packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleEntityEffect(SPacketEntityEffect packetIn) {
-
-    }
+    public void handleEntityEffect(SPacketEntityEffect packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleCombatEvent(SPacketCombatEvent packetIn) {
-
-    }
+    public void handleCombatEvent(SPacketCombatEvent packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleServerDifficulty(SPacketServerDifficulty packetIn) {
-
-    }
+    public void handleServerDifficulty(SPacketServerDifficulty packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleCamera(SPacketCamera packetIn) {
-
-    }
+    public void handleCamera(SPacketCamera packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleWorldBorder(SPacketWorldBorder packetIn) {
-
-    }
+    public void handleWorldBorder(SPacketWorldBorder packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleTitle(SPacketTitle packetIn) {
-
-    }
+    public void handleTitle(SPacketTitle packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handlePlayerListHeaderFooter(SPacketPlayerListHeaderFooter packetIn) {
-
-    }
+    public void handlePlayerListHeaderFooter(SPacketPlayerListHeaderFooter packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleResourcePack(SPacketResourcePackSend packetIn) {
-
-    }
+    public void handleResourcePack(SPacketResourcePackSend packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleUpdateBossInfo(SPacketUpdateBossInfo packetIn) {
-
-    }
+    public void handleUpdateBossInfo(SPacketUpdateBossInfo packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleCooldown(SPacketCooldown packetIn) {
-
-    }
+    public void handleCooldown(SPacketCooldown packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void handleMoveVehicle(SPacketMoveVehicle packetIn) {
-
-    }
+    public void handleMoveVehicle(SPacketMoveVehicle packetIn) {}
 
     @Override
     @ParametersAreNonnullByDefault
-    public void onDisconnect(ITextComponent reason) {
-
-    }
+    public void onDisconnect(ITextComponent reason) {}
 }
