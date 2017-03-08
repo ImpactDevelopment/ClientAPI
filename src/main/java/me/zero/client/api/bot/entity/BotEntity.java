c5c86f2c573c82aafbe26b689336c518fd58b869
Brady <bradyehahn@icloud.com>
2017-03-08 10:20:56 -0600

    Minor change to shaders, created Bot Entity

---
+++ src/main/java/me/zero/client/api/bot/entity/BotEntity.java
@@ -0,0 +1,33 @@
+package me.zero.client.api.bot.entity;
+
+import me.zero.client.api.bot.Bot;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.stats.StatisticsManager;
+import net.minecraft.world.World;
+
+/**
+ * A Bot Entity, extends EntityPlayerSP so that
+ * it can send position updates.
+ *
+ * @since 1.0
+ *
+ * Created by Brady on 3/8/2017.
+ */
+public class BotEntity extends EntityPlayerSP {
+
+    /**
+     * The bot controlling this Entity
+     */
+    private Bot bot;
+
+    public BotEntity(Minecraft mcIn, World worldIn, NetHandlerPlayClient netHandler, StatisticsManager statFile, Bot bot) {
+        super(mcIn, worldIn, netHandler, statFile);
+        this.bot = bot;
+    }
+
+    public Bot getBot() {
+        return this.bot;
+    }
+}
