ce491c2f52e326fdc27346f606e4e32dfb165ae5
Brady <zeromemesdev@gmail.com>
2017-04-05 08:48:22 -0500

    Added a Module state event

---
+++ src/main/java/me/zero/client/api/event/defaults/ModuleStateEvent.java
@@ -0,0 +1,47 @@
+package me.zero.client.api.event.defaults;
+
+import me.zero.client.api.event.type.Cancellable;
+import me.zero.client.api.module.Module;
+
+/**
+ * Called when a Module's state is changed
+ *
+ * @since 1.0
+ *
+ * Created by Brady on 4/5/2017.
+ */
+public class ModuleStateEvent extends Cancellable {
+
+    /**
+     * Module having a change in state
+     */
+    private final Module module;
+
+    /**
+     * New state of the module
+     */
+    private final boolean newState;
+
+    public ModuleStateEvent(Module module, boolean newState) {
+        this.module = module;
+        this.newState = newState;
+    }
+
+    /**
+     * @since 1.0
+     *
+     * @return The module having a change in state
+     */
+    public Module getModule() {
+        return this.module;
+    }
+
+    /**
+     * @since 1.0
+     *
+     * @return The new state of the Module
+     */
+    public boolean getNewState() {
+        return this.newState;
+    }
+}
