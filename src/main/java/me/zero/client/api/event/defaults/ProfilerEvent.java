ef5e76f29556f3ee5fe630691e674bcbc997493f
Brady <zeromemesdev@gmail.com>
2017-04-08 16:39:31 -0500

    Added Profiler-Event, Fixed code error

---
+++ src/main/java/me/zero/client/api/event/defaults/ProfilerEvent.java
@@ -0,0 +1,29 @@
+package me.zero.client.api.event.defaults;
+
+/**
+ * Called when a section is started in the profiler
+ *
+ * @since 1.0
+ *
+ * Created by Brady on 4/8/2017.
+ */
+public final class ProfilerEvent {
+
+    /**
+     * Current profiler section
+     */
+    private final String section;
+
+    public ProfilerEvent(String section) {
+        this.section = section;
+    }
+
+    /**
+     * @since 1.0
+     *
+     * @return The current profiler section
+     */
+    public String getSection() {
+        return this.section;
+    }
+}
