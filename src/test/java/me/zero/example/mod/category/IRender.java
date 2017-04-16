dc03aca3795e5fb5951fe678ef3396bbec939884
LeafHacker <leaf@leafhacker.co.uk>
2017-04-16 13:04:53 +0100

    Extract example into seperate submodule repo

--- src/test/java/me/zero/example/mod/category/IRender.java
+++
@@ -1,9 +0,0 @@
-package me.zero.example.mod.category;
-
-import me.zero.client.api.module.Category;
-
-/**
- * Created by Brady on 2/8/2017.
- */
-@Category(name = "Render")
-public interface IRender {}
