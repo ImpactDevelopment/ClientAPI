4518c8f42915bf11ca7bdb3bf44a491583fef07d
Brady <zeromemesdev@gmail.com>
2017-04-20 22:26:20 -0500

    Implemented Feather into the RenderUtils and removed Unicode Font (Temporary)

--- src/main/java/me/zero/client/api/util/render/RenderUtils.java
+++ src/main/java/me/zero/client/api/util/render/RenderUtils.java
@@ -4,14 +4,16 @@ import me.zero.client.api.util.math.Vec2;
 import me.zero.client.api.util.math.Vec3;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.OpenGlHelper;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.VertexBuffer;
+import pw.knx.feather.tessellate.GrowingTess;
+import pw.knx.feather.tessellate.OffsetTess;
 
-import static net.minecraft.client.renderer.vertex.DefaultVertexFormats.*;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.lwjgl.opengl.GL11.*;
 
 /**
- * The basic render utils for any client
+ * The basic render utils for any client,
  *
  * @since 1.0
  *
@@ -24,72 +26,54 @@ public final class RenderUtils {
     /**
      * Instance of the Tessellator
      */
-    private static Tessellator tessellator = Tessellator.getInstance();
+    private static final OffsetTess tessellator = new OffsetTess(new GrowingTess(4));
 
     /**
-     * Instance of the VertexBuffer
+     * Stores ClientState Gl Caps when setting up
      */
-    private static VertexBuffer renderer = tessellator.getBuffer();
+    private static final List<Integer> csBuffer = new ArrayList<>();
 
     /**
-     * Draws a flipped textured rectangle
+     * Called before rendering. Enables blending,
+     * line smoothing, disables 2D texturing,
+     * depth and the depth mask.
      *
      * @since 1.0
      *
-     * @param x Top corner X of the rectangle
-     * @param y Top corner Y of the rectangle
-     * @param x1 Bottom corner X of the rectangle
-     * @param y1 Bottom corner Y of the rectangle
+     * @param start true if we are starting to render, false if we are finishing
      */
-    public static void drawFlippedTexturedRect(double x, double y, double x1, double y1) {
-        renderer.begin(GL_QUADS, POSITION_TEX);
-        renderer.pos(x, y1, 0).tex(0, 0).endVertex();
-        renderer.pos(x1, y1, 0).tex(1, 0).endVertex();
-        renderer.pos(x1, y, 0).tex(1, 1).endVertex();
-        renderer.pos(x, y, 0).tex(0, 1).endVertex();
-        tessellator.draw();
+    public static void setupRender(boolean start) {
+        if (start) {
+            GlStateManager.enableBlend();
+            glEnable(GL_LINE_SMOOTH);
+            GlStateManager.disableDepth();
+            GlStateManager.disableTexture2D();
+
+            GlStateManager.blendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+            glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+        } else {
+            GlStateManager.disableBlend();
+            GlStateManager.enableTexture2D();
+            glDisable(GL_LINE_SMOOTH);
+            GlStateManager.enableDepth();
+        }
+        GlStateManager.depthMask(start);
     }
 
     /**
-     * Draws a reflected textured rectangle
+     * Enables/Disables the {@code GL_VERTEX_ARRAY} cap
      *
-     * @since 1.0
-     *
-     * @param x Top corner X of the rectangle
-     * @param y Top corner Y of the rectangle
-     * @param x1 Bottom corner X of the rectangle
-     * @param y1 Bottom corner Y of the rectangle
+     * @param enabled The new enabled state of {@code GL_VERTEX_ARRAY}
      */
-    public static void drawReflectedTexturedRect(double x, double y, double x1, double y1) {
-        renderer.begin(GL_QUADS, POSITION_TEX);
-        renderer.pos(x, y1, 0).tex(1, 0).endVertex();
-        renderer.pos(x1, y1, 0).tex(0, 0).endVertex();
-        renderer.pos(x1, y, 0).tex(0, 1).endVertex();
-        renderer.pos(x, y, 0).tex(1, 1).endVertex();
-        tessellator.draw();
-    }
+    public static void setupClientState(GlClientState state, boolean enabled) {
+        csBuffer.clear();
+        if (state.ordinal() > 0)
+            csBuffer.add(state.cap);
 
-    /**
-     * Draws a Textured Rectangle
-     *
-     * @since 1.0
-     *
-     * @param x Rect X
-     * @param y Rect Y
-     * @param width Rect Width
-     * @param height Rect Height
-     * @param u Tex U
-     * @param v Tex V
-     * @param t Tex T
-     * @param s Tex S
-     */
-    public static void drawTextureRect(float x, float y, float width, float height, float u, float v, float t, float s) {
-        renderer.begin(GL_QUADS, POSITION_TEX);
-        renderer.pos(x + width, y, 0F).tex(t, v).endVertex();
-        renderer.pos(x, y, 0F).tex(u, v).endVertex();
-        renderer.pos(x, y + height, 0F).tex(u, s).endVertex();
-        renderer.pos(x + width, y + height, 0F).tex(t, s).endVertex();
-        tessellator.draw();
+        csBuffer.add(GL_VERTEX_ARRAY);
+
+        if (enabled) csBuffer.forEach(GlStateManager::glEnableClientState);
+        else         csBuffer.forEach(GlStateManager::glDisableClientState);
     }
 
     /**
@@ -115,7 +99,7 @@ public final class RenderUtils {
      * @param width Line width
      */
     public static void drawLine(Vec3 start, Vec3 end, float width) {
-        drawLine(start.getX(), start.getY(), start.getZ(), end.getX(), end.getY(), end.getZ(), width);
+        drawLine((float) start.getX(), (float) start.getY(), (float) start.getZ(), (float) end.getX(), (float) end.getY(), (float) end.getZ(), width);
     }
 
     /**
@@ -129,7 +113,7 @@ public final class RenderUtils {
      * @param y1 End Y
      * @param width Line width
      */
-    public static void drawLine(double x, double y, double x1, double y1, float width) {
+    public static void drawLine(float x, float y, float x1, float y1, float width) {
         drawLine(x, y, 0, x1, y1, 0, width);
     }
 
@@ -146,80 +130,82 @@ public final class RenderUtils {
      * @param z1 End Z
      * @param width Line width
      */
-    public static void drawLine(double x, double y, double z, double x1, double y1, double z1, float width) {
-        preRender();
+    public static void drawLine(float x, float y, float z, float x1, float y1, float z1, float width) {
         glLineWidth(width);
-        renderer.begin(GL_LINE_STRIP, POSITION_COLOR);
-        renderer.pos(x, y, z).endVertex();
-        renderer.pos(x1, y1, z1).endVertex();
-        tessellator.draw();
-        postRender();
+
+        setupRender(true);
+        setupClientState(GlClientState.VERTEX, true);
+        tessellator.vertex(x, y, z).vertex(x1, y1, z1).draw(GL_LINE_STRIP);
+        setupClientState(GlClientState.VERTEX, false);
+        setupRender(false);
     }
 
     /**
-     * Called before rendering
+     * Draws a flipped textured rectangle
+     *
+     * @since 1.0
+     *
+     * @param x1 Top corner X of the rectangle
+     * @param y1 Top corner Y of the rectangle
+     * @param x2 Bottom corner X of the rectangle
+     * @param y2 Bottom corner Y of the rectangle
      */
-    public static void preRender() {
-        GlStateManager.enableBlend();
-        GlStateManager.blendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        GlStateManager.disableTexture2D();
-        glEnable(GL_LINE_SMOOTH);
-        glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
-        GlStateManager.disableDepth();
-        GlStateManager.depthMask(false);
+    public static void drawFlippedTexturedRect(float x1, float y1, float x2, float y2) {
+        tessellator
+                .vertex(x1, y2, 0).texture(0, 0)
+                .vertex(x2, y2, 0).texture(1, 0)
+                .vertex(x2, y1, 0).texture(1, 1)
+                .vertex(x1, y1, 0).texture(0, 1)
+                .draw(GL_QUADS);
     }
 
     /**
-     * Called after rendering
+     * Draws a reflected textured rectangle
+     *
+     * @since 1.0
+     *
+     * @param x1 Top corner X of the rectangle
+     * @param y1 Top corner Y of the rectangle
+     * @param x2 Bottom corner X of the rectangle
+     * @param y2 Bottom corner Y of the rectangle
      */
-    public static void postRender() {
-        GlStateManager.disableBlend();
-        GlStateManager.enableTexture2D();
-        glDisable(GL_LINE_SMOOTH);
-        GlStateManager.enableDepth();
-        GlStateManager.depthMask(true);
+    public static void drawReflectedTexturedRect(float x1, float y1, float x2, float y2) {
+        tessellator
+                .vertex(x1, y2, 0).texture(1, 0)
+                .vertex(x2, y2, 0).texture(1, 0)
+                .vertex(x2, y1, 0).texture(0, 1)
+                .vertex(x1, y1, 0).texture(1, 1)
+                .draw(GL_QUADS);
     }
 
-    // Normal
+    /**
+     * Renders a rectangle
+     *
+     * @since 1.0
+     *
+     * @param x1 Top corner X of the rectangle
+     * @param y1 Top corner Y of the rectangle
+     * @param x2 Bottom corner X of the rectangle
+     * @param y2 Bottom corner Y of the rectangle
+     * @param color The color of the rectangle
+     */
+    public static void rectangle(float x1, float y1, float x2, float y2, int color) {
+        GlUtils.glColor(color);
 
-    public static void rectangle(double x1, double y1, double x2, double y2, int color) {
-        if (x1 < x2) {
-            double var5 = x1;
-            x1 = x2;
-            x2 = var5;
-        }
-        if (y1 < y2) {
-            double var5 = y1;
-            y1 = y2;
-            y2 = var5;
-        }
-        float r = (color >> 16 & 0xFF) / 255.0F;
-        float g = (color >> 8 & 0xFF) / 255.0F;
-        float b = (color & 0xFF) / 255.0F;
-        float a = (color >> 24 & 0xFF) / 255.0F;
-
-        Tessellator tessellator = Tessellator.getInstance();
-        VertexBuffer worldRenderer = tessellator.getBuffer();
-        GlStateManager.enableBlend();
-        GlStateManager.disableTexture2D();
-        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-        worldRenderer.begin(GL_QUADS, POSITION_COLOR);
-        worldRenderer.pos(x1, y2, 0.0D).color(r, g, b, a).endVertex();
-        worldRenderer.pos(x2, y2, 0.0D).color(r, g, b, a).endVertex();
-        worldRenderer.pos(x2, y1, 0.0D).color(r, g, b, a).endVertex();
-        worldRenderer.pos(x1, y1, 0.0D).color(r, g, b, a).endVertex();
-        tessellator.draw();
-        GlStateManager.enableTexture2D();
-        GlStateManager.disableBlend();
+        setupRender(true);
+        setupClientState(GlClientState.VERTEX, true);
+        tessellator.vertex(x1, y2, 0).vertex(x2, y2, 0).vertex(x2, y1, 0).vertex(x1, y1, 0).draw(GL_QUADS);
+        setupClientState(GlClientState.VERTEX, false);
+        setupRender(false);
     }
 
     // Bordered
 
-    public static void rectangleBordered(double x1, double y1, double x2, double y2, int borderColor, int internalColor) {
-        rectangleBordered(x1, y1, x2, y2, 0.5, borderColor, internalColor);
+    public static void rectangleBordered(float x1, float y1, float x2, float y2, int borderColor, int internalColor) {
+        rectangleBordered(x1, y1, x2, y2, 0.5F, borderColor, internalColor);
     }
 
-    public static void rectangleBordered(double x1, double y1, double x2, double y2, double width, int borderColor, int internalColor) {
+    public static void rectangleBordered(float x1, float y1, float x2, float y2, float width, int borderColor, int internalColor) {
         rectangle(x1 + width, y1 + width, x2 - width, y2 - width, internalColor);
         rectangle(x1 + width, y1, x2 - width, y1 + width, borderColor);
         rectangle(x1, y1, x1 + width, y2, borderColor);
@@ -229,70 +215,73 @@ public final class RenderUtils {
 
     // Gradient
 
-    public static void rectangleGradient(double x1, double y1, double x2, double y2, int c1, int c2) {
+    public static void rectangleGradient(float x1, float y1, float x2, float y2, int c1, int c2) {
         rectangleGradient(x1, y1, x2, y2, new int[] { c1, c2 });
     }
 
-    private static void rectangleGradient(double x1, double y1, double x2, double y2, int[] color) {
+    private static void rectangleGradient(float x1, float y1, float x2, float y2, int[] color) {
+        if (color.length == 0)
+            throw new RuntimeException("At least one set of colors should be supplied");
+
         float[] r = new float[color.length];
         float[] g = new float[color.length];
         float[] b = new float[color.length];
         float[] a = new float[color.length];
         for (int i = 0; i < color.length; i++) {
-            r[i] = ((color[i] >> 16 & 0xFF) / 255.0F);
-            g[i] = ((color[i] >> 8 & 0xFF) / 255.0F);
-            b[i] = ((color[i] & 0xFF) / 255.0F);
-            a[i] = ((color[i] >> 24 & 0xFF) / 255.0F);
+            float[] c = Colors.getColor(color[i]);
+            r[i] = c[0]; g[i] = c[1]; b[i] = c[2]; a[i] = c[3];
         }
-        GlStateManager.disableTexture2D();
-        GlStateManager.disableBlend();
-        GlStateManager.disableAlpha();
-        OpenGlHelper.glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, 1, 0);
-        GlStateManager.blendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        GlStateManager.shadeModel(GL_FLAT);
-        Tessellator tessellator = Tessellator.getInstance();
-        VertexBuffer worldRenderer = tessellator.getBuffer();
-        worldRenderer.begin(GL_QUADS, POSITION_COLOR);
-        // Clean this up
+
         if (color.length == 1) {
-            worldRenderer.pos(x2, y1, 0.0D).color(r[0], g[0], b[0], a[0]).endVertex();
-            worldRenderer.pos(x1, y1, 0.0D).color(r[0], g[0], b[0], a[0]).endVertex();
-            worldRenderer.pos(x1, y2, 0.0D).color(r[0], g[0], b[0], a[0]).endVertex();
-            worldRenderer.pos(x2, y2, 0.0D).color(r[0], g[0], b[0], a[0]).endVertex();
-        } else if (color.length == 2 || color.length == 3) {
-            worldRenderer.pos(x2, y1, 0.0D).color(r[0], g[0], b[0], a[0]).endVertex();
-            worldRenderer.pos(x1, y1, 0.0D).color(r[0], g[0], b[0], a[0]).endVertex();
-            worldRenderer.pos(x1, y2, 0.0D).color(r[1], g[1], b[1], a[1]).endVertex();
-            worldRenderer.pos(x2, y2, 0.0D).color(r[1], g[1], b[1], a[1]).endVertex();
-        } else if (color.length >= 4) {
-            worldRenderer.pos(x2, y1, 0.0D).color(r[0], g[0], b[0], a[0]).endVertex();
-            worldRenderer.pos(x1, y1, 0.0D).color(r[1], g[1], b[1], a[1]).endVertex();
-            worldRenderer.pos(x1, y2, 0.0D).color(r[2], g[2], b[2], a[2]).endVertex();
-            worldRenderer.pos(x2, y2, 0.0D).color(r[3], g[3], b[3], a[3]).endVertex();
+            r = new float[] { r[0], r[0], r[0], r[0] };
+            g = new float[] { g[0], g[0], g[0], g[0] };
+            b = new float[] { b[0], b[0], b[0], b[0] };
+            a = new float[] { a[0], a[0], a[0], a[0] };
+        } else if (color.length > 1 && color.length < 4) {
+            r = new float[] { r[0], r[0], r[1], r[1] };
+            g = new float[] { g[0], g[0], g[1], g[1] };
+            b = new float[] { b[0], b[0], b[1], b[1] };
+            a = new float[] { a[0], a[0], a[1], a[1] };
+        } else {
+            r = new float[] { r[0], r[1], r[2], r[3] };
+            g = new float[] { g[0], g[1], g[2], g[3] };
+            b = new float[] { b[0], b[1], b[2], b[3] };
+            a = new float[] { a[0], a[1], a[2], a[3] };
         }
-        tessellator.draw();
+
+        setupRender(true);
+        OpenGlHelper.glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, 1, 0);
         GlStateManager.shadeModel(GL_FLAT);
-        GlStateManager.disableBlend();
-        GlStateManager.enableAlpha();
-        GlStateManager.enableTexture2D();
+        setupClientState(GlClientState.COLOR, true);
+
+        tessellator
+                .color(r[0], g[0], b[0], a[0]).vertex(x1, y2, 0)
+                .color(r[1], g[1], b[1], a[1]).vertex(x2, y2, 0)
+                .color(r[2], g[2], b[2], a[2]).vertex(x2, y1, 0)
+                .color(r[3], g[3], b[3], a[3]).vertex(x1, y1, 0)
+                .draw(GL_QUADS);
+
+        setupClientState(GlClientState.COLOR, false);
+        GlStateManager.shadeModel(GL_FLAT);
+        setupRender(false);
     }
 
     // Bordered Gradient
 
-    public static void rectangleBorderedGradient(double x1, double y1, double x2, double y2, int border, int c1, int c2) {
-        rectangleBorderedGradient(x1, y1, x2, y2, border, c1, c2, 0.5);
+    public static void rectangleBorderedGradient(float x1, float y1, float x2, float y2, int border, int c1, int c2) {
+        rectangleBorderedGradient(x1, y1, x2, y2, border, c1, c2, 0.5F);
     }
 
-    public static void rectangleBorderedGradient(double x1, double y1, double x2, double y2, int border, int c1, int c2, double width) {
+    public static void rectangleBorderedGradient(float x1, float y1, float x2, float y2, int border, int c1, int c2, float width) {
         rectangleBorderedGradient(x1, y1, x2, y2, new int[] { c1, c2 }, new int[] { border }, width);
     }
 
-    public static void rectangleBorderedGradient(double x1, double y1, double x2, double y2, int[] fill, int[] outline, double width) {
+    public static void rectangleBorderedGradient(float x1, float y1, float x2, float y2, int[] fill, int[] outline, float width) {
         rectangleOutlinedGradient(x1, y1, x2, y2, outline, width);
         rectangleGradient(x1 + width, y1 + width, x2 - width, y2 - width, fill);
     }
 
-    public static void rectangleOutlinedGradient(double x1, double y1, double x2, double y2, int[] color, double width) {
+    public static void rectangleOutlinedGradient(float x1, float y1, float x2, float y2, int[] color, float width) {
         rectangleGradient(x1, y1, x2, y1 + width, color);
         rectangleGradient(x1, y2 - width, x2, y2, color);
         rectangleGradient(x1, y1 + width, x1 + width, y2 - width, color);
