72d546d9bb1d1ecdd4e7d49df429e0137f1b42f7
Brady <bradyehahn@icloud.com>
2017-05-09 18:12:05 -0500

    Updated LayerRenderEvent (Called for individual layers)

--- src/main/java/me/zero/client/load/mixin/MixinRenderLivingBase.java
+++ src/main/java/me/zero/client/load/mixin/MixinRenderLivingBase.java
@@ -5,11 +5,12 @@ import me.zero.client.api.event.defaults.EntityRenderEvent;
 import me.zero.client.api.event.defaults.LayerRenderEvent;
 import me.zero.client.api.event.type.EventState;
 import net.minecraft.client.renderer.entity.RenderLivingBase;
-import net.minecraft.entity.Entity;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.entity.EntityLivingBase;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
@@ -28,11 +29,12 @@ public class MixinRenderLivingBase {
         EventManager.post(new EntityRenderEvent(EventState.POST, (RenderLivingBase) (Object) this, entity));
     }
 
-    @Inject(method = "renderLayers", at = @At("HEAD"), cancellable = true)
-    public void renderLayers(EntityLivingBase entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scaleIn, CallbackInfo ci) {
-        LayerRenderEvent event = new LayerRenderEvent(entitylivingbaseIn);
+    @Redirect(method = "renderLayers", at = @At(value = "INVOKE", target = "net/minecraft/client/renderer/entity/layers/LayerRenderer.doRenderLayer(Lnet/minecraft/entity/EntityLivingBase;FFFFFFF)V"))
+    @SuppressWarnings("unchecked")
+    public void doRenderLayer(LayerRenderer renderer, EntityLivingBase entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scaleIn) {
+        LayerRenderEvent event = new LayerRenderEvent(entitylivingbaseIn, renderer);
         EventManager.post(event);
-        if (event.isCancelled())
-            ci.cancel();
+        if (!event.isCancelled())
+            renderer.doRenderLayer(entitylivingbaseIn, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch, scaleIn);
     }
 }
