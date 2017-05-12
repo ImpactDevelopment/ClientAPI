2d2578bbb77f8cf94c953e316851307ac9fe201f
Brady <zeromemesdev@gmail.com>
2017-05-11 20:25:35 -0500

    Fixed excessive Vec2 and Vec3 instantiation

--- src/main/java/me/zero/client/api/util/math/Vec3.java
+++ src/main/java/me/zero/client/api/util/math/Vec3.java
@@ -20,6 +20,10 @@ public final class Vec3 {
 
     private double x, y, z;
 
+    public Vec3() {
+        this(0, 0, 0);
+    }
+
     public Vec3(double x, double y, double z) {
         this.x = x;
         this.y = y;
