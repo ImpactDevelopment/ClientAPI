53b745e8ace8fedac81e96164a3e1c7bda9dfa0f
LeafHacker <leafhacker@yahoo.com>
2017-06-05 19:09:13 +0100

    Use Accessors for CPacketPlayer
    Example of how Accessors can be used in a wrapper interface instead of
    separately implementing them in a mixin.
    
    You can use @Accessor (for fields) and @Invoker (for methods) in
    @Mixin interfaces to gain access to private members.
    
    You can also have non-mixin members as usual.
    
    If the name of the accessor target is not easily parsed from the method
    name, you can specify it as the annotation value.
    
     You can have static accessors, but since static methods must have a
     body, mixin will replace any body you provide.

--- src/main/java/me/zero/client/load/mixin/wrapper/ICPacketPlayer.java
+++ src/main/java/me/zero/client/load/mixin/wrapper/ICPacketPlayer.java
@@ -1,11 +1,16 @@
 package me.zero.client.load.mixin.wrapper;
 
+import net.minecraft.network.play.client.CPacketPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
 /**
  * Used to set the Player Packet fields
  *
  * @author Brady
  * @since 2/24/2017 12:00 PM
  */
+@Mixin(CPacketPlayer.class)
 public interface ICPacketPlayer {
 
     /**
@@ -13,40 +18,111 @@ public interface ICPacketPlayer {
      *
      * @param x X coordinate
      */
-    void setX(double x);
+    @Accessor void setX(double x);
+
+    /**
+     * Gets the packet X coordinate
+     *
+     * @return X coordinate
+     */
+    @Accessor double getX();
 
     /**
      * Sets the packet Y coordinate
      *
      * @param y Y coordinate
      */
-    void setY(double y);
+    @Accessor void setY(double y);
+
+    /**
+     * Gets the packet Y coordinate
+     *
+     * @return Y coordinate
+     */
+    @Accessor double getY();
 
     /**
      * Sets the packet Z coordinate
      *
      * @param z Z coordinate
      */
-    void setZ(double z);
+    @Accessor void setZ(double z);
+
+    /**
+     * Gets the packet Z coordinate
+     *
+     * @return Z coordinate
+     */
+    @Accessor double getZ();
 
     /**
      * Sets the packet yaw rotation
      *
      * @param yaw Yaw rotation
      */
-    void setYaw(float yaw);
+    @Accessor void setYaw(float yaw);
+
+    /**
+     * Sets the packet yaw rotation
+     *
+     * @return Yaw rotation
+     */
+    @Accessor float getYaw();
 
     /**
      * Sets the packet pitch rotation
      *
      * @param pitch Pitch rotation
      */
-    void setPitch(float pitch);
+    @Accessor void setPitch(float pitch);
+
+    /**
+     * Gets the packet pitch rotation
+     *
+     * @return Pitch rotation
+     */
+    @Accessor float getPitch();
 
     /**
      * Sets the packet onGround state
      *
      * @param onGround onGround state
      */
-    void setOnGround(boolean onGround);
+    @Accessor void setOnGround(boolean onGround);
+
+    /**
+     * Gets the packet onGround state
+     *
+     * @return onGround state
+     */
+    @Accessor boolean getOnGround();
+
+    /**
+     * Sets the packet moving state
+     *
+     * @param moving moving state
+     */
+    @Accessor void setMoving(boolean moving);
+
+    /**
+     * Gets the packet moving state
+     *
+     * @return moving state
+     */
+    @Accessor boolean getMoving();
+
+    /**
+     * Sets the packet rotating state
+     *
+     * @param rotating rotating state
+     */
+    @Accessor void setRotating(boolean rotating);
+
+    /**
+     * Gets the packet rotating state
+     *
+     * @return rotating state
+     */
+    @Accessor boolean getRotating();
+
 }
