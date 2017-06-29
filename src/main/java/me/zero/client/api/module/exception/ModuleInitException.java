ba929e7b689396c0f409c2e378bacefca004aadb
Brady <zeromemesdev@gmail.com>
2017-06-29 15:23:59 -0500

    Additional Module and Command constructors
    Parameters that would be passed through an annotation can now be passed through a constructor for both Modules and Commands

---
+++ src/main/java/me/zero/client/api/module/exception/ModuleInitException.java
@@ -0,0 +1,12 @@
+package me.zero.client.api.module.exception;
+
+/**
+ * @author Brady
+ * @since 6/29/2017 3:05 PM
+ */
+public final class ModuleInitException extends RuntimeException {
+
+    public ModuleInitException(String message) {
+        super(message);
+    }
+}
