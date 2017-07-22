29e2512d5ff997878f1bfb634a90cebc25714f34
Brady <zeromemesdev@gmail.com>
2017-07-22 17:52:28 -0500

    Removed unused code, Created OpenGL objects
    Added "GlObject", a way to easily interface with OpenGL objects in memory.
    Made a "DisplayList" object for OpenGL display lists.
    Used the new DisplayList in WidgetHandler

--- src/main/java/me/zero/client/api/util/Messages.java
+++ src/main/java/me/zero/client/api/util/Messages.java
@@ -17,13 +17,15 @@
 package me.zero.client.api.util;
 
 import me.zero.client.api.util.logger.Level;
+import me.zero.client.api.util.logger.ILogger;
 
 /**
  * Storage for all Messages used by the Logger's logf method
  * as well as some generic messages sent to the client from
  * the api.
  *
- * @see me.zero.client.api.util.logger.ILogger#logf(Level, String, Object...)
+ * @see ILogger#log(Level, String)
+ * @see ILogger#logf(Level, String, Object...)
  *
  * @author Brady
  * @since 1/21/2017 12:00 PM
@@ -36,9 +38,4 @@ public interface Messages {
     String PLUGIN_CANT_CREATE_MODULE           = "Unable to create Module, %s";
     String PLUGIN_CANT_LOAD_CLASS              = "Unable to load Class, %s";
     String PLUGIN_CANT_CREATE_INPUTSTREAM      = "Unable to create jar InputStream, %s";
-
-    String MODULE_INSTANTIATION                = "Unable to instantiate Module, %s";
-
-    String COMMAND_MISSING_ARGS                = "Missing required argument: %s, with type %s";
-    String COMMAND_INVALID                     = "Invalid Command";
 }
