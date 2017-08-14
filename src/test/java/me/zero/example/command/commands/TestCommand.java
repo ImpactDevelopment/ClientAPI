a376fbd3b96269d3a974b192d1edd54ba801fb92
Brady <zeromemesdev@gmail.com>
2017-08-14 17:31:55 +0100

    Ditch submodules and import the example repo
    Remove all traces of submodules and instead track all the example code
    here in the main repo.
    
    This commit is by @LeafHacker, but @ZeroMemes is listed as the author
    because he is the author of the code imported from the example repo.

---
+++ src/test/java/me/zero/example/command/commands/TestCommand.java
@@ -0,0 +1,26 @@
+package me.zero.example.command.commands;
+
+import me.zero.client.api.command.Cmd;
+import me.zero.client.api.command.Command;
+import me.zero.client.api.command.exception.CommandException;
+import me.zero.client.api.command.exception.InvalidSyntaxException;
+import me.zero.client.api.command.executor.sender.CommandSender;
+import net.minecraft.util.text.TextComponentString;
+
+import java.util.Arrays;
+
+/**
+ * @author Brady
+ * @since 5/31/2017 6:18 PM
+ */
+@Cmd(headers = { "test", "example"}, description = "Test Command")
+public final class TestCommand extends Command {
+
+    @Override
+    public void execute(CommandSender sender, String[] arguments) throws CommandException {
+        // Print a chat message indicating the success of the
+        mc.ingameGUI.getChatGUI().printChatMessage(new TextComponentString(
+                String.format("%s executed the \"Test\" command with arguments %s", sender.getName(), Arrays.toString(arguments))
+        ));
+    }
+}
