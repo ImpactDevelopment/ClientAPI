b1feb87b89e76fe4b03faaedfe7ba489c083f5ed
Brady <zeromemesdev@gmail.com>
2017-08-23 22:06:24 -0500

    Mixin wrapper for all client packets

---
+++ src/main/java/clientapi/load/mixin/packet/client/ICPacketPlayerDigging.java
@@ -0,0 +1,27 @@
+package clientapi.load.mixin.packet.client;
+
+import net.minecraft.network.play.client.CPacketPlayerDigging;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+/**
+ * @author Brady
+ * @since 8/23/2017 9:07 PM
+ */
+@Mixin(CPacketPlayerDigging.class)
+public interface ICPacketPlayerDigging {
+
+    @Accessor BlockPos getPosition();
+
+    @Accessor void setPosition(BlockPos position);
+
+    @Accessor EnumFacing getFacing();
+
+    @Accessor void setFacing(EnumFacing facing);
+
+    @Accessor CPacketPlayerDigging.Action getAction();
+
+    @Accessor void setAction(CPacketPlayerDigging.Action action);
+}
---
+++ src/main/java/clientapi/load/mixin/packet/client/ICPacketPlayerTryUseItemOnBlock.java
@@ -0,0 +1,40 @@
+package clientapi.load.mixin.packet.client;
+
+import net.minecraft.network.play.client.CPacketPlayerTryUseItemOnBlock;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+/**
+ * @author Brady
+ * @since 8/23/2017 9:14 PM
+ */
+@Mixin(CPacketPlayerTryUseItemOnBlock.class)
+public interface ICPacketPlayerTryUseItemOnBlock {
+
+    @Accessor BlockPos getPosition();
+
+    @Accessor void setPosition(BlockPos position);
+
+    @Accessor EnumFacing getPlacedBlockDirection();
+
+    @Accessor void setPlacedBlockDirection(EnumFacing placedBlockDirection);
+
+    @Accessor EnumHand getHand();
+
+    @Accessor void getHand(EnumHand hand);
+
+    @Accessor float getFacingX();
+
+    @Accessor void setFacingX(float facingX);
+
+    @Accessor float getFacingY();
+
+    @Accessor void setFacingY(float facingY);
+
+    @Accessor float getFacingZ();
+
+    @Accessor void setFacingZ();
+}
