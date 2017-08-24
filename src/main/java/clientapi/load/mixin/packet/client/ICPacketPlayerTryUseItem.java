b0150881906df60029ada2856b0dc4cedf0b3652
Brady <ZeroMemes@users.noreply.github.com>
2017-08-23 22:06:24 -0500

    Mixin wrapper for all client packets

---
+++ src/main/java/clientapi/load/mixin/packet/client/ICPacketPlayerTryUseItem.java
@@ -0,0 +1,18 @@
+package clientapi.load.mixin.packet.client;
+
+import net.minecraft.network.play.client.CPacketPlayerTryUseItem;
+import net.minecraft.util.EnumHand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+/**
+ * @author Brady
+ * @since 8/23/2017 9:12 PM
+ */
+@Mixin(CPacketPlayerTryUseItem.class)
+public interface ICPacketPlayerTryUseItem {
+
+    @Accessor EnumHand getHand();
+
+    @Accessor void setHand(EnumHand hand);
+}
---
+++ src/main/java/clientapi/load/mixin/packet/client/ICPacketRecipeInfo.java
@@ -0,0 +1,30 @@
+package clientapi.load.mixin.packet.client;
+
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.network.play.client.CPacketRecipeInfo;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+/**
+ * @author Brady
+ * @since 8/23/2017 9:28 PM
+ */
+@Mixin(CPacketRecipeInfo.class)
+public interface ICPacketRecipeInfo {
+
+    @Accessor CPacketRecipeInfo.Purpose getPurpose();
+
+    @Accessor void setPurpose(CPacketRecipeInfo.Purpose purpose);
+
+    @Accessor IRecipe getRecipe();
+
+    @Accessor void setRecipe(IRecipe recipe);
+
+    @Accessor("isGuiOpen") boolean isGuiOpen();
+
+    @Accessor("isGuiOpen") void setGuiOpen();
+
+    @Accessor boolean isFilteringCraftable();
+
+    @Accessor void setIsFilteringCraftable(boolean isFilteringCraftable);
+}
