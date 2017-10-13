a98ce6281999b3fa5fa69e396f89e6e82e923d74
Brady <ZeroMemes@users.noreply.github.com>
2017-10-13 09:32:47 -0500

    GLenum Update

---
+++ src/main/java/clientapi/util/render/gl/glenum/GLenum.java
@@ -0,0 +1,18 @@
+package clientapi.util.render.gl.glenum;
+
+/**
+ * @author Brady
+ * @since 10/12/2017 3:30 PM
+ */
+public interface GLenum {
+
+    /**
+     * @return The name of the cap
+     */
+    String getName();
+
+    /**
+     * @return The integer value of the cap
+     */
+    int getCap();
+}
