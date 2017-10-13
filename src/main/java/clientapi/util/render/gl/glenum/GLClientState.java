3c0204e2e57300500f6c4a0ab2ff3a71285757a7
Brady <ZeroMemes@users.noreply.github.com>
2017-10-13 09:32:47 -0500

    GLenum Update

--- src/main/java/clientapi/util/render/gl/glenum/GLClientState.java
+++ src/main/java/clientapi/util/render/gl/glenum/GLClientState.java
@@ -17,16 +17,41 @@
 package clientapi.util.render.gl.glenum;
 
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL14.*;
+import static org.lwjgl.opengl.GL15.*;
 
-public enum GLClientState {
+/**
+ * Contains all valid GLenum ClientState caps.
+ *
+ * @see <a href="https://www.khronos.org/registry/OpenGL-Refpages/gl2.1/xhtml/glEnableClientState.xml">glEnableClientState</a>
+ * @see <a href="https://www.khronos.org/registry/OpenGL-Refpages/gl2.1/xhtml/glDisableClientState.xml">glDisableClientState</a>
+ */
+public enum GLClientState implements GLenum {
 
-    VERTEX(GL_VERTEX_ARRAY),
-    COLOR(GL_COLOR_ARRAY),
-    TEXTURE(GL_TEXTURE_COORD_ARRAY);
+    COLOR("GL_COLOR_ARRAY", GL_COLOR_ARRAY),
+    EDGE("GL_EDGE_FLAG_ARRAY", GL_EDGE_FLAG_ARRAY),
+    FOG("GL_FOG_COORD_ARRAY", GL_FOG_COORD_ARRAY),
+    INDEX("GL_INDEX_ARRAY", GL_INDEX_ARRAY),
+    NORMAL("GL_NORMAL_ARRAY", GL_NORMAL_ARRAY),
+    SECONDARY_COLOR("GL_SECONDARY_COLOR_ARRAY", GL_SECONDARY_COLOR_ARRAY),
+    TEXTURE("GL_TEXTURE_COORD_ARRAY", GL_TEXTURE_COORD_ARRAY),
+    VERTEX("GL_VERTEX_ARRAY", GL_VERTEX_ARRAY);
 
-    public final int cap;
+    private final String name;
+    private final int cap;
 
-    GLClientState(int cap) {
+    GLClientState(String name, int cap) {
+        this.name = name;
         this.cap = cap;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getCap() {
+        return this.cap;
     }
 }
