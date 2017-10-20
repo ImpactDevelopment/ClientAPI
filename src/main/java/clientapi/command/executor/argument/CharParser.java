a3ce4ce7a10bbe8b9cca7097b12575c5074a2830
Brady <ZeroMemes@users.noreply.github.com>
2017-10-20 11:06:12 -0500

    Created CharParser

---
+++ src/main/java/clientapi/command/executor/argument/CharParser.java
@@ -0,0 +1,23 @@
+package clientapi.command.executor.argument;
+
+import clientapi.command.executor.ExecutionContext;
+
+/**
+ * @author Brady
+ * @since 10/20/2017 11:04 AM
+ */
+public final class CharParser implements ArgumentParser<Character> {
+
+    @Override
+    public final Character parse(ExecutionContext context, Class type, String raw) {
+        if (raw.length() == 1) {
+            return raw.charAt(0);
+        }
+        return null;
+    }
+
+    @Override
+    public final boolean isTarget(Class type) {
+        return Character.class.isAssignableFrom(type) || Character.TYPE.isAssignableFrom(type);
+    }
+}
