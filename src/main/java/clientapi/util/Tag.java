dc75c06fc3f67ff342662ee85c5f6334ac8c11e5
Brady <ZeroMemes@users.noreply.github.com>
2017-11-01 15:27:58 -0500

    Tag javadoc update

--- src/main/java/clientapi/util/Tag.java
+++ src/main/java/clientapi/util/Tag.java
@@ -1,8 +1,13 @@
 package clientapi.util;
 
 import clientapi.util.interfaces.Identifiable;
+import clientapi.util.interfaces.Taggable;
 
 /**
+ * A tag. Refer to {@code Taggable} for usage.
+ *
+ * @see Taggable
+ *
  * @author Brady
  * @since 11/1/2017 2:29 PM
  */
@@ -28,11 +33,23 @@ public final class Tag<T> implements Identifiable {
         this.id = id;
     }
 
+    /**
+     * Gets the value of this tag.
+     *
+     * @param <U> The type to cast the value to
+     * @return The value of this tag.
+     */
     @SuppressWarnings("unchecked")
     public final <U> U getValue() {
         return (U) this.value;
     }
 
+    /**
+     * Sets the value of this tag.
+     *
+     * @param value The new value
+     * @param <U> The type to cast to the value type
+     */
     @SuppressWarnings("unchecked")
     public final <U> void setValue(U value) {
         this.value = (T) value;
