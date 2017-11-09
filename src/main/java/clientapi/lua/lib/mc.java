4d8e5d29f82bb6ac0361ce67413767be33dc8aa6
Brady <zeromemesdev@gmail.com>
2017-11-08 22:32:51 -0600

    Created LuaLibrary class

--- src/main/java/clientapi/lua/lib/mc.java
+++ src/main/java/clientapi/lua/lib/mc.java
@@ -1,12 +1,14 @@
 package clientapi.lua.lib;
 
+import clientapi.lua.LuaLibrary;
 import clientapi.lua.extension.FourArgFunction;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.gui.FontRenderer;
 import org.luaj.vm2.LuaInteger;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.lib.OneArgFunction;
-import org.luaj.vm2.lib.TwoArgFunction;
+
+import java.util.Map;
 
 import static clientapi.util.interfaces.Helper.*;
 
@@ -14,19 +16,17 @@ import static clientapi.util.interfaces.Helper.*;
  * @author Brady
  * @since 11/8/2017 12:26 PM
  */
-public final class mc extends TwoArgFunction {
+public final class mc extends LuaLibrary {
+
+    public mc() {
+        super("mc");
+    }
 
     @Override
-    public final LuaValue call(LuaValue modname, LuaValue env) {
-        LuaValue library = tableOf();
-
-        // Setup functions
-        library.set("message", new message());
-        library.set("drawString", new drawString());
-        library.set("drawStringWithShadow", new drawStringWithShadow());
-
-        env.set("mc", library);
-        return library;
+    public final void load(Map<String, LuaValue> table) {
+        table.put("message", new message());
+        table.put("drawString", new drawString());
+        table.put("drawStringWithShadow", new drawStringWithShadow());
     }
 
     /**
