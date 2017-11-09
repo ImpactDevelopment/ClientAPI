7948587e5c43079d1384b2ce090628523f677353
Brady <ZeroMemes@users.noreply.github.com>
2017-11-09 07:41:08 -0600

    Renamed "hook.create" to "hook.Add"

--- src/example/resources/lua/example.lua
+++ src/example/resources/lua/example.lua
@@ -2,6 +2,6 @@ require("clientapi/lua/lib/mc")
 require("clientapi/lua/lib/render")
 require("clientapi/lua/lib/hook")
 
-hook.create("RenderHudEvent", function()
+hook.Add("RenderHudEvent", function()
     render.DrawTextWithShadow("Script Running", 2, 14, 0xFFFFFFFF)
 end)
