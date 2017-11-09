e8bca3022f6b580dd46c6dccc069f02e9a797a1f
Brady <ZeroMemes@users.noreply.github.com>
2017-11-08 22:45:23 -0600

    Give render functions separate library

--- src/example/resources/lua/example.lua
+++ src/example/resources/lua/example.lua
@@ -1,6 +1,7 @@
 require("clientapi/lua/lib/mc")
+require("clientapi/lua/lib/render")
 require("clientapi/lua/lib/hook")
 
 hook.create("RenderHudEvent", function()
-    mc.drawStringWithShadow("Script Running", 2, 14, 0xFFFFFFFF)
+    render.DrawText("Script Running", 2, 14, 0xFFFFFFFF)
 end)
