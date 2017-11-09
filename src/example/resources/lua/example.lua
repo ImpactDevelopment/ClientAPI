08c9508e0721a1dd6257328360c9cc3c43c8cb89
Brady <ZeroMemes@users.noreply.github.com>
2017-11-09 15:11:37 -0600

    Removed unnecessary libs from example script

--- src/example/resources/lua/example.lua
+++ src/example/resources/lua/example.lua
@@ -1,7 +1,3 @@
-require("clientapi/lua/lib/mc")
-require("clientapi/lua/lib/render")
-require("clientapi/lua/lib/hook")
-
 hook.Add("RenderHudEvent", function()
     render.DrawTextWithShadow("Script Running", 2, 14, 0xFFFFFFFF)
 end)
