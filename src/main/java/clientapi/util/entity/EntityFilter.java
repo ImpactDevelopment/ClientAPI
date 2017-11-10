ad49dfcd3b9feb2cae255f8fe24610ffb9f9456c
Brady <ZeroMemes@users.noreply.github.com>
2017-11-09 21:29:08 -0600

    EntityFilter javadoc updates

--- src/main/java/clientapi/util/entity/EntityFilter.java
+++ src/main/java/clientapi/util/entity/EntityFilter.java
@@ -3,11 +3,18 @@ package clientapi.util.entity;
 import net.minecraft.entity.Entity;
 
 /**
+ * {@code FunctionalInterface} to determine the validity
+ * of an entity based on this filter's standards.
+ *
  * @author Brady
  * @since 11/7/2017 12:15 PM
  */
 @FunctionalInterface
 public interface EntityFilter {
 
+    /**=
+     * @param entity The entity to validate
+     * @return Whether or not the specified entity is valid to this filter's standards.
+     */
     boolean isValid(Entity entity);
 }
