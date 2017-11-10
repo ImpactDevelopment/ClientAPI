12417030874fea8a3303bc73319a83003519fe22
Brady <ZeroMemes@users.noreply.github.com>
2017-11-09 21:29:08 -0600

    EntityFilter javadoc updates

--- src/main/java/clientapi/util/entity/MultiEntityFilter.java
+++ src/main/java/clientapi/util/entity/MultiEntityFilter.java
@@ -9,11 +9,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
+ * {@code EntityFilter} implementation that can have multiple child entity filters.
+ *
  * @author Brady
  * @since 11/7/2017 12:14 PM
  */
 public final class MultiEntityFilter implements EntityFilter {
 
+    /**
+     * The child {@code EntityFilters} that are run when checking the validity of an {@code Entity}
+     */
     private final List<EntityFilter> filters = new ArrayList<>();
 
     public MultiEntityFilter() {}
@@ -31,6 +36,42 @@ public final class MultiEntityFilter implements EntityFilter {
         return this.filters.stream().filter(filter -> !filter.isValid(entity)).count() > 0;
     }
 
+    /**
+     * Adds the specified filter to this {@code MultiEntityFilter}
+     * if it is not already a child filter.
+     *
+     * @param filter The filter to add
+     */
+    public final void addFilter(EntityFilter filter) {
+        if (!this.filters.contains(filter))
+            this.filters.add(filter);
+    }
+
+    /**
+     * Removes the specified filter from this {@code MultiEntityFilter}
+     * if it is already a child filter.
+     *
+     * @param filter The filter to add
+     */
+    public final void removeFilter(EntityFilter filter) {
+        if (this.filters.contains(filter))
+            this.filters.remove(filter);
+    }
+
+    /**
+     * @return A copy of this {@code MultiEntityFilter's} child filters.
+     */
+    public final List<EntityFilter> getFilters() {
+        return new ArrayList<>(this.filters);
+    }
+
+    /**
+     * Filters the specified collection of entities with all of this
+     * {@code MultiEntityFilter's} child filters, and returns the filtered list.
+     *
+     * @param entities The collection to be filtered
+     * @return A filtered list
+     */
     public final List<Entity> filter(Collection<Entity> entities) {
         return entities.stream().filter(this::isValid).collect(Collectors.toList());
     }
