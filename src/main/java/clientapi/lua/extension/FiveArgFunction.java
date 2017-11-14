4769d9fc1192a436b8abf2bf9c95fe561b2b0b89
Brady <ZeroMemes@users.noreply.github.com>
2017-11-14 11:53:59 -0600

    Created lua FiveArgFunction

---
+++ src/main/java/clientapi/lua/extension/FiveArgFunction.java
@@ -0,0 +1,25 @@
+package clientapi.lua.extension;
+
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.Varargs;
+import org.luaj.vm2.lib.LibFunction;
+
+/**
+ * Abstract base class for Java function implementations
+ * that take five arguments and return one value.
+ *
+ * @author Brady
+ * @since 11/8/2017 6:05 PM
+ */
+abstract public class FiveArgFunction extends LibFunction {
+
+    abstract public LuaValue call(LuaValue arg1, LuaValue arg2, LuaValue arg3, LuaValue arg4, LuaValue arg5);
+
+    /** Default constructor */
+    public FiveArgFunction() {
+    }
+
+    public final Varargs invoke(Varargs varargs) {
+        return call(varargs.arg1(), varargs.arg(2), varargs.arg(3), varargs.arg(4), varargs.arg(5));
+    }
+}
