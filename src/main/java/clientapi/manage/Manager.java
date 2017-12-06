7fb75042dc0d70823857e52758b68d907c1e77c7
Brady <zeromemesdev@gmail.com>
2017-12-06 12:13:36 -0600

    Cleaned up the new implementation of Manager

--- src/main/java/clientapi/manage/Manager.java
+++ src/main/java/clientapi/manage/Manager.java
@@ -19,25 +19,25 @@ package clientapi.manage;
 import clientapi.util.interfaces.Saveable;
 import clientapi.util.interfaces.Loadable;
 
-import javax.annotation.Nonnull;
 import java.util.*;
 
 /**
- * Manager used to store arrays of information.
- * Just a way to keep things consistent.
+ * Managers are used to store arrays of objects. {@code Manager}
+ * extends {@code ArrayList}, allowing full access to the Manager's
+ * contents. The usage of the manager is intended to store implementations
+ * of the generic argument <T>, ex) in the context of modules or commands.
+ * A {@code get(Class)} method is provided, allowing the access of entries
+ * by their class type.
  *
  * @see Loadable
  * @see Saveable
  *
+ * @param <T> The type of entries that this manager holds
+ *
  * @author Brady
  * @since 1/19/2017 12:00 PM
  */
-public abstract class Manager<T> implements List<T>, Loadable, Saveable {
-
-    /**
-     * The list of all of the entries that this Manager contains
-     */
-    private final List<T> data = new ArrayList<>();
+public abstract class Manager<T> extends ArrayList<T> implements Loadable, Saveable {
 
     /**
      * Cache of classes and their corresponding entries
@@ -65,7 +65,7 @@ public abstract class Manager<T> implements List<T>, Loadable, Saveable {
             return null;
 
         return (I) classCache.computeIfAbsent((Class<T>) clazz,
-                c -> this.data.stream().filter(data -> data.getClass().equals(c)).findFirst().orElse(null));
+                c -> this.stream().filter(data -> data.getClass().equals(c)).findFirst().orElse(null));
     }
 
     /**
@@ -73,125 +73,5 @@ public abstract class Manager<T> implements List<T>, Loadable, Saveable {
      */
     public final String getName() {
         return this.name;
-    }
-
-    @SafeVarargs
-    public final boolean addAll(T... c) {
-        return this.addAll(Arrays.asList(c));
-    }
-
-    @Override
-    public final boolean addAll(int index, Collection<? extends T> c) {
-        return this.data.addAll(index, c);
-    }
-
-    @Override
-    public final T get(int index) {
-        return this.data.get(index);
-    }
-
-    @Override
-    public final T set(int index, T element) {
-        return this.data.set(index, element);
-    }
-
-    @Override
-    public final void add(int index, T element) {
-        this.data.add(index, element);
-    }
-
-    @Override
-    public final T remove(int index) {
-        return this.data.remove(index);
-    }
-
-    @Override
-    public final int indexOf(Object o) {
-        return this.data.indexOf(o);
-    }
-
-    @Override
-    public final int lastIndexOf(Object o) {
-        return this.data.lastIndexOf(o);
-    }
-
-    @Override
-    public final ListIterator<T> listIterator() {
-        return this.data.listIterator();
-    }
-
-    @Override
-    public final ListIterator<T> listIterator(int index) {
-        return this.data.listIterator(index);
-    }
-
-    @Override
-    public final List<T> subList(int fromIndex, int toIndex) {
-        return this.data.subList(fromIndex, toIndex);
-    }
-
-    @Override
-    public final int size() {
-        return this.data.size();
-    }
-
-    @Override
-    public final boolean isEmpty() {
-        return this.data.isEmpty();
-    }
-
-    @Override
-    public final boolean contains(Object o) {
-        return this.data.contains(o);
-    }
-
-    @Override
-    public final Object[] toArray() {
-        return this.data.toArray();
-    }
-
-    @Override
-    public final <T1> T1[] toArray(T1[] a) {
-        return this.data.toArray(a);
-    }
-
-    @Override
-    public final boolean add(T t) {
-        return this.data.add(t);
-    }
-
-    @Override
-    public final boolean remove(Object o) {
-        return this.data.remove(o);
-    }
-
-    @Override
-    public final boolean containsAll(Collection<?> c) {
-        return this.data.containsAll(c);
-    }
-
-    @Override
-    public final boolean addAll(Collection<? extends T> c) {
-        return this.data.addAll(c);
-    }
-
-    @Override
-    public final boolean removeAll(Collection<?> c) {
-        return this.data.removeAll(c);
-    }
-
-    @Override
-    public final boolean retainAll(Collection<?> c) {
-        return this.data.retainAll(c);
-    }
-
-    @Override
-    public final void clear() {
-        this.data.clear();
-    }
-
-    @Override
-    public final Iterator<T> iterator() {
-        return this.data.iterator();
     }
 }
