32fced1c58656c935c8a789719fba4e57dd932aa
Brady <ZeroMemes@users.noreply.github.com>
2018-01-04 11:20:10 -0600

    Refactored a few command exception class names

--- src/main/java/clientapi/command/exception/InvalidSyntaxException.java
+++ src/main/java/clientapi/command/exception/InvalidSyntaxException.java
@@ -1,33 +1,18 @@
-/*
- * Copyright 2017 ImpactDevelopment
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package clientapi.command.exception;
 
 import clientapi.command.Command;
 
 /**
- * Thrown by implementations of Command if the provided
- * arguments aren't valid. Hence, invalid synax.
- *
  * @author Brady
- * @since 5/31/2017 9:13 AM
+ * @since 10/18/2017 11:07 AM
  */
 public final class InvalidSyntaxException extends CommandException {
 
     public InvalidSyntaxException(Command command) {
-        super(command, "Invalid Command Syntax");
+        super(command, "An invalid amount of arguments were provided");
+    }
+
+    public InvalidSyntaxException(Command command, int found, int expected) {
+        super(command, found > expected ? "Too many arguments provided" : found < expected ? "Not enough arguments provided" : "Correct amount of arguments provided, unknown cause.");
     }
 }
