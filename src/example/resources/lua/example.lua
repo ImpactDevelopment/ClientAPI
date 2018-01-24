9219ffb0e272d8eaa39bd206e4f0a4e5eecc0b28
Brady <ZeroMemes@users.noreply.github.com>
2018-01-24 14:36:36 -0600

    Cleanup ClientHandler imports

--- src/example/resources/lua/example.lua
+++ src/example/resources/lua/example.lua
@@ -1,3 +1,4 @@
-hook.Add("RenderHudEvent", "example_script_hud", function()
+-- A basic script to draw "Script Running" in the top left of the screen
+hook.Add("RenderHudEvent", "example_script_hud", function(event)
     render.DrawTextWithShadow("Script Running", 2, 14, 0xFFFFFFFF)
 end)
