b2735944c5f40287000c47804fd75887723b9549
Brady <ZeroMemes@users.noreply.github.com>
2018-02-05 15:15:06 -0600

    Improved the EntityFilter system

---
+++ src/main/java/clientapi/util/entity/EntityCheck.java
@@ -0,0 +1,24 @@
+package clientapi.util.entity;
+
+import net.minecraft.entity.Entity;
+
+/**
+ * {@code FunctionalInterface} to determine the validity
+ * of an entity based on this filter's standards.
+ *
+ * @author Brady
+ * @since 11/7/2017 12:15 PM
+ */
+public interface EntityCheck {
+
+    /**
+     * @param entity The entity to validate
+     * @return Whether or not the specified entity is valid to this filter's standards
+     */
+    boolean isValid(Entity entity);
+
+    /**
+     * @return The type of this filter
+     */
+    CheckType getType();
+}
--- src/main/java/clientapi/util/entity/EntityFilter.java
+++ src/main/java/clientapi/util/entity/EntityFilter.java
@@ -2,19 +2,82 @@ package clientapi.util.entity;
 
 import net.minecraft.entity.Entity;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
- * {@code FunctionalInterface} to determine the validity
- * of an entity based on this filter's standards.
+ * {@code EntityFilter} implementation that can have multiple child entity filters.
  *
  * @author Brady
- * @since 11/7/2017 12:15 PM
+ * @since 11/7/2017 12:14 PM
  */
-@FunctionalInterface
-public interface EntityFilter {
+public final class EntityFilter implements EntityCheck {
 
-    /**=
-     * @param entity The entity to validate
-     * @return Whether or not the specified entity is valid to this filter's standards.
+    /**
+     * The child {@code EntityFilters} that are run when checking the validity of an {@code Entity}
      */
-    boolean isValid(Entity entity);
+    private final List<EntityCheck> checks = new ArrayList<>();
+
+    public EntityFilter() {}
+
+    public EntityFilter(EntityCheck... checks) {
+        this(Arrays.asList(checks));
+    }
+
+    public EntityFilter(Iterable<EntityCheck> checks) {
+        checks.forEach(this.checks::add);
+    }
+
+    @Override
+    public final boolean isValid(Entity entity) {
+        return entity != null
+                && this.checks.stream().filter(filter -> filter.getType() == CheckType.RESTRICT).allMatch(filter -> filter.isValid(entity))
+                && this.checks.stream().filter(filter -> filter.getType() == CheckType.ALLOW).anyMatch(filter -> filter.isValid(entity));
+
+    }
+
+    @Override
+    public final CheckType getType() {
+        return CheckType.RESTRICT;
+    }
+
+    /**
+     * Adds the specified check to this {@code EntityFilter}
+     * if it is not already a child filter.
+     *
+     * @param filter The filter to add
+     */
+    public final void addFilter(EntityCheck filter) {
+        if (!this.checks.contains(filter))
+            this.checks.add(filter);
+    }
+
+    /**
+     * Removes the specified check from this {@code EntityFilter}
+     * if it is already a child filter.
+     *
+     * @param filter The filter to add
+     */
+    public final void removeFilter(EntityCheck filter) {
+        if (this.checks.contains(filter))
+            this.checks.remove(filter);
+    }
+
+    /**
+     * @return A copy of this {@code EntityFilter's} child checks.
+     */
+    public final List<EntityCheck> getChecks() {
+        return new ArrayList<>(this.checks);
+    }
+
+    /**
+     * Filters the specified collection of entities with all of this
+     * {@code EntityFilter's} checks, and returns the filtered list.
+     *
+     * @param entities The collection to be filtered
+     * @return A filtered list
+     */
+    public final List<Entity> filter(Collection<Entity> entities) {
+        return entities.stream().filter(this::isValid).collect(Collectors.toList());
+    }
 }
