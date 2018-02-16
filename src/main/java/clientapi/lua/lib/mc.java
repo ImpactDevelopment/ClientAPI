c6321c2b31050df1ca3beebc3b4368ebba430ca3
Brady <zeromemesdev@gmail.com>
2018-02-15 20:54:10 -0600

    Update copyright notices

--- src/main/java/clientapi/lua/lib/mc.java
+++ src/main/java/clientapi/lua/lib/mc.java
@@ -1,16 +1,29 @@
+/*
+ * Copyright 2017 ImpactDevelopment
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package clientapi.lua.lib;
 
 import clientapi.lua.LuaLibrary;
-import clientapi.lua.extension.FourArgFunction;
 import net.minecraft.client.entity.EntityPlayerSP;
-import net.minecraft.client.gui.FontRenderer;
-import org.luaj.vm2.LuaInteger;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.lib.OneArgFunction;
 
 import java.util.Map;
 
-import static clientapi.util.interfaces.Helper.*;
+import static clientapi.util.interfaces.Helper.mc;
 
 /**
  * @author Brady
