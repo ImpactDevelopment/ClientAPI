b41d84358e572b3ea47e7f63ffbc92b0e579d1d0
Brady <zeromemesdev@gmail.com>
2018-04-21 09:36:18 -0500

    Update the version to 3.0.1

--- src/main/java/clientapi/Version.java
+++ src/main/java/clientapi/Version.java
@@ -40,7 +40,7 @@ public final class Version {
     /**
      * Incremented every release, reset when the minor version is modified
      */
-    public static final int PATCH = 0;
+    public static final int PATCH = 1;
 
     /**
      * @return The version formatted as {@code MAJOR.MINOR.PATCH}
