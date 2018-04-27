aee5b7e866ad3a69e6a983381f3c086e74ffb3a3
LeafHacker <leafhacker@yahoo.com>
2018-04-28 00:07:08 +0100

    Replace all @Overwrite mixins
    WARNING! Potential fire and explosions:
    
    Untested code that probably breaks everything and makes kittens cry...

--- src/main/java/clientapi/load/mixin/MixinEntityPlayerSP.java
+++ src/main/java/clientapi/load/mixin/MixinEntityPlayerSP.java
@@ -29,11 +29,9 @@ import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.entity.MoverType;
 import net.minecraft.network.play.client.CPacketChatMessage;
-import net.minecraft.network.play.client.CPacketEntityAction;
-import net.minecraft.network.play.client.CPacketPlayer;
+import net.minecraft.util.math.AxisAlignedBB;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -48,20 +46,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class MixinEntityPlayerSP extends MixinEntityLivingBase {
 
     @Shadow @Final public NetHandlerPlayClient connection;
-    @Shadow private double lastReportedPosX;
-    @Shadow private double lastReportedPosY;
-    @Shadow private double lastReportedPosZ;
-    @Shadow private float lastReportedYaw;
-    @Shadow private float lastReportedPitch;
-    @Shadow private boolean serverSprintState;
-    @Shadow private boolean serverSneakState;
-    @Shadow private boolean prevOnGround;
-    @Shadow private int positionUpdateTicks;
     @Shadow protected Minecraft mc;
-    @Shadow private boolean autoJumpEnabled;
 
-    @Shadow public abstract boolean isSneaking();
-    @Shadow protected abstract boolean isCurrentViewEntity();
+    private MotionUpdateEvent preMotionUpdateEvent;
 
     @Inject(method = "onUpdate", at = @At("HEAD"))
     private void onPreUpdate(CallbackInfo ci) {
@@ -106,78 +93,44 @@ public abstract class MixinEntityPlayerSP extends MixinEntityLivingBase {
         this.connection.sendPacket(new CPacketChatMessage(event.getRawMessage()));
     }
 
-    /**
-     * @reason In addition to firing pre and post events, we also want to override some position values (prefixed with p).
-     * @author Brady
-     */
-    @Overwrite
-    private void onUpdateWalkingPlayer() {
-        EntityPlayerSP _this = (EntityPlayerSP) (Object) this;
+    @Inject(method = "onUpdateWalkingPlayer", at = @At("HEAD"))
+    private void onPreUpdateWalking(CallbackInfo ci) {
+        ClientAPI.EVENT_BUS.post(this.preMotionUpdateEvent = new MotionUpdateEvent(EventState.PRE));
+    }
 
-        MotionUpdateEvent pre = new MotionUpdateEvent(EventState.PRE);
-        ClientAPI.EVENT_BUS.post(pre);
-
-        boolean clientSprintState = this.isSprinting();
-        if (clientSprintState != this.serverSprintState) {
-            this.connection.sendPacket(new CPacketEntityAction(_this, clientSprintState ? CPacketEntityAction.Action.START_SPRINTING : CPacketEntityAction.Action.STOP_SPRINTING));
-            this.serverSprintState = clientSprintState;
-        }
-
-        boolean clientSneakState = this.isSneaking();
-        if (clientSneakState != this.serverSneakState) {
-            this.connection.sendPacket(new CPacketEntityAction(_this, clientSneakState ? CPacketEntityAction.Action.START_SNEAKING : CPacketEntityAction.Action.STOP_SNEAKING));
-            this.serverSneakState = clientSneakState;
-        }
-
-        if (this.isCurrentViewEntity()) {
-
-            // Override vanilla defaults of this.posX, this.posY, etc.
-            // This is why we need to overwrite the method body.
-            double pX = pre.getX();
-            double pY = pre.getY();
-            double pZ = pre.getZ();
-            float pYaw = pre.getYaw();
-            float pPitch = pre.getPitch();
-            boolean pGround = pre.isOnGround();
-
-            double d0 = pX - this.lastReportedPosX;
-            double d1 = pY - this.lastReportedPosY;
-            double d2 = pZ - this.lastReportedPosZ;
-            double d3 = pYaw - this.lastReportedYaw;
-            double d4 = pPitch - this.lastReportedPitch;
-
-            boolean position = d0 * d0 + d1 * d1 + d2 * d2 > 9.0E-4D || ++this.positionUpdateTicks >= 20;
-            boolean rotation = d3 != 0.0D || d4 != 0.0D;
-
-            if (this.isRiding()) {
-                this.connection.sendPacket(new CPacketPlayer.PositionRotation(this.motionX, -999.0D, this.motionZ, this.rotationYaw, this.rotationPitch, this.onGround));
-                position = false;
-            } else if (position && rotation) {
-                this.connection.sendPacket(new CPacketPlayer.PositionRotation(pX, pY, pZ, pYaw, pPitch, pGround));
-            } else if (position) {
-                this.connection.sendPacket(new CPacketPlayer.Position(pX, pY, pZ, pGround));
-            } else if (rotation) {
-                this.connection.sendPacket(new CPacketPlayer.Rotation(pYaw, pPitch, pGround));
-            } else if (this.prevOnGround != pGround) {
-                this.connection.sendPacket(new CPacketPlayer(pGround));
-            }
-
-            if (position) {
-                this.lastReportedPosX = pX;
-                this.lastReportedPosY = pY;
-                this.lastReportedPosZ = pZ;
-                this.positionUpdateTicks = 0;
-            }
-
-            if (rotation) {
-                this.lastReportedYaw = pYaw;
-                this.lastReportedPitch = pPitch;
-            }
-
-            this.prevOnGround = pGround;
-            this.autoJumpEnabled = this.mc.gameSettings.autoJump;
-        }
-
+    @Inject(method = "onUpdateWalkingPlayer", at = @At("RETURN"))
+    private void onPostUpdateWalking(CallbackInfo ci) {
         ClientAPI.EVENT_BUS.post(new MotionUpdateEvent(EventState.POST));
+        this.preMotionUpdateEvent = null;
+    }
+
+    @Redirect(method = "onUpdateWalkingPlayer", at = @At(value = "FIELD", target = "Lnet/minecraft/client/entity/EntityPlayerSP;posX:D"))
+    private double injectPosX(EntityPlayerSP self) {
+        return this.preMotionUpdateEvent.getX();
+    }
+
+    @Redirect(method = "onUpdateWalkingPlayer", at = @At(value = "FIELD", target = "Lnet/minecraft/util/math/AxisAlignedBB;minY:D"))
+    private double injectPosY(AxisAlignedBB self) {
+        return this.preMotionUpdateEvent.getY();
+    }
+
+    @Redirect(method = "onUpdateWalkingPlayer", at = @At(value = "FIELD", target = "Lnet/minecraft/client/entity/EntityPlayerSP;posZ:D"))
+    private double injectPosZ(EntityPlayerSP self) {
+        return this.preMotionUpdateEvent.getZ();
+    }
+
+    @Redirect(method = "onUpdateWalkingPlayer", at = @At(value = "FIELD", target = "Lnet/minecraft/client/entity/EntityPlayerSP;rotationYaw:F"))
+    private float injectYaw(EntityPlayerSP self) {
+        return this.preMotionUpdateEvent.getYaw();
+    }
+
+    @Redirect(method = "onUpdateWalkingPlayer", at = @At(value = "FIELD", target = "Lnet/minecraft/client/entity/EntityPlayerSP;rotationPitch:F"))
+    private float injectPitch(EntityPlayerSP self) {
+        return this.preMotionUpdateEvent.getPitch();
+    }
+
+    @Redirect(method = "onUpdateWalkingPlayer", at = @At(value = "FIELD", target = "Lnet/minecraft/client/entity/EntityPlayerSP;onGround:Z"))
+    private boolean injectOnGround(EntityPlayerSP self) {
+        return this.preMotionUpdateEvent.isOnGround();
     }
 }
