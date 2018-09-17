862fdcfbfdb417b634d5122ebde488b5e6ea5f5f
Brady <zeromemesdev@gmail.com>
2018-09-17 18:38:43 -0500

    Clean up the GLClientState management in RenderUtils

--- src/main/java/clientapi/util/render/gl/glenum/GLClientState.java
+++ src/main/java/clientapi/util/render/gl/glenum/GLClientState.java
@@ -17,8 +17,6 @@
 package clientapi.util.render.gl.glenum;
 
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL14.GL_SECONDARY_COLOR_ARRAY;
-import static org.lwjgl.opengl.GL15.GL_FOG_COORD_ARRAY;
 
 /**
  * Contains all valid GLenum ClientState caps.
@@ -28,14 +26,9 @@ import static org.lwjgl.opengl.GL15.GL_FOG_COORD_ARRAY;
  */
 public enum GLClientState implements GLenum {
 
-    COLOR("GL_COLOR_ARRAY", GL_COLOR_ARRAY),
-    EDGE("GL_EDGE_FLAG_ARRAY", GL_EDGE_FLAG_ARRAY),
-    FOG("GL_FOG_COORD_ARRAY", GL_FOG_COORD_ARRAY),
-    INDEX("GL_INDEX_ARRAY", GL_INDEX_ARRAY),
-    NORMAL("GL_NORMAL_ARRAY", GL_NORMAL_ARRAY),
-    SECONDARY_COLOR("GL_SECONDARY_COLOR_ARRAY", GL_SECONDARY_COLOR_ARRAY),
+    COLOR  ("GL_COLOR_ARRAY",         GL_COLOR_ARRAY),
     TEXTURE("GL_TEXTURE_COORD_ARRAY", GL_TEXTURE_COORD_ARRAY),
-    VERTEX("GL_VERTEX_ARRAY", GL_VERTEX_ARRAY);
+    VERTEX ("GL_VERTEX_ARRAY",        GL_VERTEX_ARRAY);
 
     private final String name;
     private final int cap;
